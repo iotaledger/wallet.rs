f4159186c9e2ef31334035e657bb2a3ecbd7193b
Lucas Nogueira <lucas@tauri.studio>
2020-08-31 18:40:57 -0300

    feat(storage) implement official stronghold interface

--- src/storage/stronghold.rs
+++ src/storage/stronghold.rs
@@ -1,14 +1,14 @@
-mod interface;
-
 use super::sqlite::SqliteStorageAdapter;
 use super::StorageAdapter;
 use crate::account::AccountIdentifier;
-use std::path::{Path, PathBuf};
+use std::path::Path;
+
+use stronghold::{Base64Decodable, Id as StrongholdId, Stronghold};
 
 /// Stronghold storage adapter.
 pub struct StrongholdStorageAdapter {
-    storage_path: PathBuf,
     id_storage: SqliteStorageAdapter,
+    stronghold: Stronghold,
 }
 
 impl StrongholdStorageAdapter {
@@ -19,25 +19,33 @@ impl StrongholdStorageAdapter {
         let storage_path = path.as_ref().join("snapshot");
         let adapter = Self {
             id_storage,
-            storage_path,
+            stronghold: Stronghold::new(storage_path),
         };
         Ok(adapter)
     }
 }
 
+fn create_stronghold_id(id: String) -> crate::Result<StrongholdId> {
+    let bytes = Vec::from_base64(id.as_bytes())?;
+    let id = StrongholdId::load(&bytes)?;
+    Ok(id)
+}
+
 impl StorageAdapter for StrongholdStorageAdapter {
     fn get(&self, account_id: AccountIdentifier) -> crate::Result<String> {
-        let stronghold_id = self.id_storage.get(account_id)?;
-        let account = interface::read(&self.storage_path, "password", stronghold_id);
-        Ok(account.expect("failed to read account"))
+        let stronghold_id_string = self.id_storage.get(account_id)?;
+        let stronghold_id = create_stronghold_id(stronghold_id_string)?;
+        let account = self.stronghold.record_read(&stronghold_id, "password");
+        Ok(account)
     }
 
     fn get_all(&self) -> crate::Result<std::vec::Vec<String>> {
         let mut accounts = vec![];
-        let ids = interface::list_ids(&self.storage_path, "password");
+        let ids = self.id_storage.get_all()?;
         for id in ids {
-            let account = interface::read(&self.storage_path, "password", id);
-            accounts.push(account.expect("failed to read account"));
+            let id = create_stronghold_id(id)?;
+            let account = self.stronghold.record_read(&id, "password");
+            accounts.push(account);
         }
         Ok(accounts)
     }
@@ -47,15 +55,19 @@ impl StorageAdapter for StrongholdStorageAdapter {
         account_id: AccountIdentifier,
         account: String,
     ) -> std::result::Result<(), anyhow::Error> {
-        let stronghold_id = interface::encrypt(&self.storage_path, "password", &account);
+        let stronghold_id = self
+            .stronghold
+            .record_create("", account.as_str(), "password");
         self.id_storage
             .set(account_id, format!("{:?}", stronghold_id))?;
         Ok(())
     }
 
     fn remove(&self, account_id: AccountIdentifier) -> std::result::Result<(), anyhow::Error> {
-        let stronghold_id = self.id_storage.get(account_id)?;
-        interface::revoke(&self.storage_path, "password", stronghold_id);
+        let stronghold_id_string = self.id_storage.get(account_id.clone())?;
+        let stronghold_id = create_stronghold_id(stronghold_id_string)?;
+        self.stronghold.record_remove(stronghold_id, "password");
+        self.id_storage.remove(account_id)?;
         Ok(())
     }
 }
