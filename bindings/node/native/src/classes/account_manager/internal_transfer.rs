e4d5453364ca0d417a06e3319ed76993e36555a4
Lucas Fernandes Nogueira <lucas@tauri.studio>
2020-12-03 12:11:44 -0300

    fix(stronghold): do not allow multiple reads on the same thread, closes #143 (#144)

--- bindings/node/native/src/classes/account_manager/internal_transfer.rs
+++ bindings/node/native/src/classes/account_manager/internal_transfer.rs
@@ -15,47 +15,47 @@ use iota_wallet::{account_manager::AccountManager, message::Message, WalletError
 use neon::prelude::*;
 
 pub struct InternalTransferTask {
-  pub manager: Arc<RwLock<AccountManager>>,
-  pub from_account_id: String,
-  pub to_account_id: String,
-  pub amount: u64,
+    pub manager: Arc<RwLock<AccountManager>>,
+    pub from_account_id: String,
+    pub to_account_id: String,
+    pub amount: u64,
 }
 
 impl Task for InternalTransferTask {
-  type Output = Message;
-  type Error = WalletError;
-  type JsEvent = JsValue;
+    type Output = Message;
+    type Error = WalletError;
+    type JsEvent = JsValue;
 
-  fn perform(&self) -> Result<Self::Output, Self::Error> {
-    let manager = self.manager.read().unwrap();
-    crate::block_on(crate::convert_async_panics(|| async {
-      let from_account = crate::get_account(&self.from_account_id);
-      let from_account = from_account.read().unwrap();
-      let to_account = crate::get_account(&self.to_account_id);
-      let to_account = to_account.read().unwrap();
-      let res = manager
-        .internal_transfer(
-          from_account.id().into(),
-          to_account.id().into(),
-          self.amount,
-        )
-        .await?;
+    fn perform(&self) -> Result<Self::Output, Self::Error> {
+        let manager = self.manager.read().unwrap();
+        crate::block_on(crate::convert_async_panics(|| async {
+            let from_account = crate::get_account(&self.from_account_id);
+            let from_account = from_account.read().unwrap();
+            let to_account = crate::get_account(&self.to_account_id);
+            let to_account = to_account.read().unwrap();
+            let res = manager
+                .internal_transfer(
+                    from_account.id().into(),
+                    to_account.id().into(),
+                    self.amount,
+                )
+                .await?;
 
-      crate::update_account(&self.from_account_id, res.from_account);
-      crate::update_account(&self.to_account_id, res.to_account);
+            crate::update_account(&self.from_account_id, res.from_account);
+            crate::update_account(&self.to_account_id, res.to_account);
 
-      Ok(res.message)
-    }))
-  }
-
-  fn complete(
-    self,
-    mut cx: TaskContext,
-    value: Result<Self::Output, Self::Error>,
-  ) -> JsResult<Self::JsEvent> {
-    match value {
-      Ok(val) => Ok(neon_serde::to_value(&mut cx, &val)?),
-      Err(e) => cx.throw_error(e.to_string()),
+            Ok(res.message)
+        }))
     }
-  }
+
+    fn complete(
+        self,
+        mut cx: TaskContext,
+        value: Result<Self::Output, Self::Error>,
+    ) -> JsResult<Self::JsEvent> {
+        match value {
+            Ok(val) => Ok(neon_serde::to_value(&mut cx, &val)?),
+            Err(e) => cx.throw_error(e.to_string()),
+        }
+    }
 }
