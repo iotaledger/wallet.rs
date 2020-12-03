091efbc0dac81d641cae33d2b18e487e68589aae
Lucas Nogueira <lucas@tauri.studio>
2020-12-03 16:27:42 -0300

    feat(storage): use stronghold interface with block_on

--- src/storage/stronghold.rs
+++ src/storage/stronghold.rs
@@ -30,24 +30,30 @@ impl StrongholdStorageAdapter {
 
 impl StorageAdapter for StrongholdStorageAdapter {
     fn get(&self, account_id: AccountIdentifier) -> crate::Result<String> {
-        crate::stronghold::get_account(&self.path, account_id);
-        unimplemented!()
+        crate::block_on(async {
+            let account = crate::stronghold::get_account(&self.path, account_id).await?;
+            Ok(account)
+        })
     }
 
     fn get_all(&self) -> crate::Result<std::vec::Vec<String>> {
-        crate::stronghold::get_accounts(&self.path);
-        unimplemented!()
+        crate::block_on(async {
+            let accounts = crate::stronghold::get_accounts(&self.path).await?;
+            Ok(accounts)
+        })
     }
 
     fn set(&self, account_id: AccountIdentifier, account: String) -> crate::Result<()> {
-        // crate::stronghold::store_(&self.path, account_id, account);
-
-        Ok(())
+        crate::block_on(async {
+            crate::stronghold::store_account(&self.path, account_id, account).await?;
+            Ok(())
+        })
     }
 
     fn remove(&self, account_id: AccountIdentifier) -> crate::Result<()> {
-        crate::stronghold::remove_account(&self.path, account_id);
-
-        Ok(())
+        crate::block_on(async {
+            crate::stronghold::remove_account(&self.path, account_id).await?;
+            Ok(())
+        })
     }
 }
