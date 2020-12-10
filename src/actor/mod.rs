2de6d70748eb2aa629b3d4b2bcf0121b69a743cb
Lucas Nogueira <lucas@tauri.studio>
2020-12-10 00:52:20 -0300

    perf(lib): cleanup clones on actor interface

--- src/actor/mod.rs
+++ src/actor/mod.rs
@@ -2,15 +2,16 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::{
-    account::AccountIdentifier,
+    account::{Account, AccountIdentifier},
     account_manager::AccountManager,
-    message::{Message as WalletMessage, Transfer},
+    message::Transfer,
     DateTime, Result, Utc,
 };
 use futures::{Future, FutureExt};
 use iota::message::prelude::MessageId;
 use std::{
     any::Any,
+    collections::HashMap,
     convert::TryInto,
     panic::{catch_unwind, AssertUnwindSafe},
     path::PathBuf,
@@ -23,17 +24,20 @@ pub use message::*;
 /// The Wallet message handler.
 pub struct WalletMessageHandler {
     account_manager: AccountManager,
+    // TODO use this as a proper cache mechanism
+    accounts: HashMap<AccountIdentifier, Account>,
 }
 
 impl Default for WalletMessageHandler {
     fn default() -> Self {
         Self {
             account_manager: AccountManager::new().unwrap(),
+            accounts: Default::default(),
         }
     }
 }
 
-fn panic_to_response_message(panic: Box<dyn Any>) -> Result<ResponseType> {
+fn panic_to_response_message<'a>(panic: Box<dyn Any>) -> Result<ResponseType<'a>> {
     let msg = if let Some(message) = panic.downcast_ref::<String>() {
         format!("Internal error: {}", message)
     } else if let Some(message) = panic.downcast_ref::<&str>() {
@@ -45,16 +49,16 @@ fn panic_to_response_message(panic: Box<dyn Any>) -> Result<ResponseType> {
     Ok(ResponseType::Panic(format!("{}\n\n{:?}", msg, current_backtrace)))
 }
 
-fn convert_panics<F: FnOnce() -> Result<ResponseType>>(f: F) -> Result<ResponseType> {
+fn convert_panics<'a, F: FnOnce() -> Result<ResponseType<'a>>>(f: F) -> Result<ResponseType<'a>> {
     match catch_unwind(AssertUnwindSafe(|| f())) {
         Ok(result) => result,
         Err(panic) => panic_to_response_message(panic),
     }
 }
 
-async fn convert_async_panics<F>(f: impl FnOnce() -> F) -> Result<ResponseType>
+async fn convert_async_panics<'a, F>(f: impl FnOnce() -> F) -> Result<ResponseType<'a>>
 where
-    F: Future<Output = Result<ResponseType>>,
+    F: Future<Output = Result<ResponseType<'a>>>,
 {
     match AssertUnwindSafe(f()).catch_unwind().await {
         Ok(result) => result,
@@ -62,11 +66,12 @@ where
     }
 }
 
-impl WalletMessageHandler {
+impl<'a> WalletMessageHandler {
     /// Creates a new instance of the message handler with the default account manager.
     pub fn new() -> Result<Self> {
         let instance = Self {
             account_manager: AccountManager::new()?,
+            accounts: Default::default(),
         };
         Ok(instance)
     }
@@ -75,6 +80,7 @@ impl WalletMessageHandler {
     pub fn with_storage_path(storage_path: PathBuf) -> Result<Self> {
         let instance = Self {
             account_manager: AccountManager::with_storage_path(storage_path)?,
+            accounts: Default::default(),
         };
         Ok(instance)
     }
@@ -85,32 +91,39 @@ impl WalletMessageHandler {
     }
 
     /// Handles a message.
-    pub async fn handle(&mut self, message: Message) {
-        let response: Result<ResponseType> = match message.message_type() {
-            MessageType::RemoveAccount(account_id) => convert_panics(|| self.remove_account(account_id)),
-            MessageType::CreateAccount(account) => convert_panics(|| self.create_account(account)),
-            MessageType::GetAccount(account_id) => convert_panics(|| self.get_account(account_id)),
-            MessageType::GetAccounts => convert_panics(|| self.get_accounts()),
+    pub async fn handle(&'a mut self, message: Message<'a>) {
+        let response: Result<ResponseType<'_>> = match message.message_type() {
+            MessageType::RemoveAccount(account_id) => convert_panics(move || self.remove_account(account_id)),
+            MessageType::CreateAccount(account) => convert_panics(move || self.create_account(account)),
+            MessageType::GetAccount(account_id) => convert_panics(move || self.get_account(account_id)),
+            MessageType::GetAccounts => convert_panics(move || self.get_accounts()),
             MessageType::CallAccountMethod { account_id, method } => {
-                convert_async_panics(|| async { self.call_account_method(account_id, method).await }).await
+                convert_async_panics(move || async move { self.call_account_method(account_id, method).await }).await
             }
-            MessageType::SyncAccounts => convert_async_panics(|| async { self.sync_accounts().await }).await,
+            MessageType::SyncAccounts => convert_async_panics(move || async move { self.sync_accounts().await }).await,
             MessageType::Reattach { account_id, message_id } => {
-                convert_async_panics(|| async { self.reattach(account_id, message_id).await }).await
+                convert_async_panics(move || async move { self.reattach(account_id, message_id).await }).await
             }
-            MessageType::Backup(destination_path) => convert_panics(|| self.backup(destination_path)),
-            MessageType::RestoreBackup(backup_path) => convert_panics(|| self.restore_backup(backup_path)),
-            MessageType::SetStrongholdPassword(password) => convert_panics(|| self.set_stronghold_password(password)),
+            MessageType::Backup(destination_path) => convert_panics(move || self.backup(destination_path)),
+            MessageType::RestoreBackup(backup_path) => convert_panics(move || self.restore_backup(backup_path)),
+            MessageType::SetStrongholdPassword(password) => {
+                convert_panics(move || self.set_stronghold_password(password))
+            }
             MessageType::SendTransfer { account_id, transfer } => {
-                convert_async_panics(|| async { self.send_transfer(account_id, transfer).await }).await
+                convert_async_panics(move || async move { self.send_transfer(account_id, transfer).await }).await
             }
             MessageType::InternalTransfer {
                 from_account_id,
                 to_account_id,
                 amount,
             } => {
-                convert_async_panics(|| async { self.internal_transfer(from_account_id, to_account_id, *amount).await })
-                    .await
+                let from_account_id = from_account_id.clone();
+                let to_account_id = to_account_id.clone();
+                let amount = *amount;
+                convert_async_panics(move || async move {
+                    self.internal_transfer(&from_account_id, &to_account_id, amount).await
+                })
+                .await
             }
         };
 
@@ -123,17 +136,17 @@ impl WalletMessageHandler {
             .send(Response::new(message.id().to_string(), message.message_type, response));
     }
 
-    fn backup(&self, destination_path: &str) -> Result<ResponseType> {
+    fn backup(&self, destination_path: &str) -> Result<ResponseType<'_>> {
         self.account_manager.backup(destination_path)?;
         Ok(ResponseType::BackupSuccessful)
     }
 
-    fn restore_backup(&self, backup_path: &str) -> Result<ResponseType> {
+    fn restore_backup(&self, backup_path: &str) -> Result<ResponseType<'_>> {
         self.account_manager.import_accounts(backup_path)?;
         Ok(ResponseType::BackupRestored)
     }
 
-    async fn reattach(&self, account_id: &AccountIdentifier, message_id: &str) -> Result<ResponseType> {
+    async fn reattach(&self, account_id: &AccountIdentifier, message_id: &str) -> Result<ResponseType<'_>> {
         let parsed_message_id = MessageId::new(
             message_id.as_bytes()[..]
                 .try_into()
@@ -143,17 +156,20 @@ impl WalletMessageHandler {
         Ok(ResponseType::Reattached(message_id.to_string()))
     }
 
-    async fn sync_accounts(&self) -> Result<ResponseType> {
+    async fn sync_accounts(&self) -> Result<ResponseType<'_>> {
         let synced = self.account_manager.sync_accounts().await?;
         Ok(ResponseType::SyncedAccounts(synced))
     }
 
     async fn call_account_method(
-        &self,
+        &mut self,
         account_id: &AccountIdentifier,
         method: &AccountMethod,
-    ) -> Result<ResponseType> {
-        let mut account = self.account_manager.get_account(account_id)?;
+    ) -> Result<ResponseType<'_>> {
+        let account = self.account_manager.get_account(account_id)?;
+        self.accounts.insert(account_id.clone(), account);
+        let account = self.accounts.get_mut(account_id).unwrap();
+
         match method {
             AccountMethod::GenerateAddress => {
                 let address = account.generate_address()?;
@@ -164,20 +180,16 @@ impl WalletMessageHandler {
                 from,
                 message_type,
             } => {
-                let messages: Vec<WalletMessage> = account
-                    .list_messages(*count, *from, message_type.clone())
-                    .into_iter()
-                    .cloned()
-                    .collect();
+                let messages = account.list_messages(*count, *from, message_type.clone());
                 Ok(ResponseType::Messages(messages))
             }
             AccountMethod::ListAddresses { unspent } => {
-                let addresses = account.list_addresses(*unspent).into_iter().cloned().collect();
+                let addresses = account.list_addresses(*unspent);
                 Ok(ResponseType::Addresses(addresses))
             }
             AccountMethod::GetAvailableBalance => Ok(ResponseType::AvailableBalance(account.available_balance())),
             AccountMethod::GetTotalBalance => Ok(ResponseType::TotalBalance(account.total_balance())),
-            AccountMethod::GetLatestAddress => Ok(ResponseType::LatestAddress(account.latest_address().cloned())),
+            AccountMethod::GetLatestAddress => Ok(ResponseType::LatestAddress(account.latest_address())),
             AccountMethod::SyncAccount {
                 address_index,
                 gap_limit,
@@ -202,14 +214,14 @@ impl WalletMessageHandler {
     }
 
     /// The remove account message handler.
-    fn remove_account(&self, account_id: &AccountIdentifier) -> Result<ResponseType> {
+    fn remove_account(&self, account_id: &AccountIdentifier) -> Result<ResponseType<'_>> {
         self.account_manager
             .remove_account(&account_id)
             .map(|_| ResponseType::RemovedAccount(account_id.clone()))
     }
 
     /// The create account message handler.
-    fn create_account(&self, account: &AccountToCreate) -> Result<ResponseType> {
+    fn create_account(&self, account: &AccountToCreate) -> Result<ResponseType<'_>> {
         let mut builder = self.account_manager.create_account(account.client_options.clone());
 
         if let Some(mnemonic) = &account.mnemonic {
@@ -229,22 +241,25 @@ impl WalletMessageHandler {
         builder.initialise().map(ResponseType::CreatedAccount)
     }
 
-    fn get_account(&self, account_id: &AccountIdentifier) -> Result<ResponseType> {
+    fn get_account(&self, account_id: &AccountIdentifier) -> Result<ResponseType<'_>> {
         let account = self.account_manager.get_account(&account_id)?;
         Ok(ResponseType::ReadAccount(account))
     }
 
-    fn get_accounts(&self) -> Result<ResponseType> {
+    fn get_accounts(&mut self) -> Result<ResponseType<'_>> {
         let accounts = self.account_manager.get_accounts()?;
-        Ok(ResponseType::ReadAccounts(accounts))
+        for account in accounts {
+            self.accounts.insert(account.id().clone(), account);
+        }
+        Ok(ResponseType::ReadAccounts(self.accounts.values().collect()))
     }
 
-    fn set_stronghold_password(&mut self, password: &str) -> Result<ResponseType> {
+    fn set_stronghold_password(&mut self, password: &str) -> Result<ResponseType<'_>> {
         self.account_manager.set_stronghold_password(password)?;
         Ok(ResponseType::StrongholdPasswordSet)
     }
 
-    async fn send_transfer(&self, account_id: &AccountIdentifier, transfer: &Transfer) -> Result<ResponseType> {
+    async fn send_transfer(&self, account_id: &AccountIdentifier, transfer: &Transfer) -> Result<ResponseType<'_>> {
         let mut account = self.account_manager.get_account(account_id)?;
         let synced = account.sync().execute().await?;
         let message = synced.transfer(transfer.clone()).await?.message;
@@ -256,7 +271,7 @@ impl WalletMessageHandler {
         from_account_id: &AccountIdentifier,
         to_account_id: &AccountIdentifier,
         amount: u64,
-    ) -> Result<ResponseType> {
+    ) -> Result<ResponseType<'_>> {
         let message = self
             .account_manager
             .internal_transfer(from_account_id, to_account_id, amount)
