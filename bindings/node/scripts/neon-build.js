add83da89c98bf197d07fd1fdc44893a24cd621e
Rajiv Shah <rajivshah1@icloud.com>
2021-01-25 01:40:05 -0500

    Revert "refactor(bindings): nodejs binding prebuild-less"
    This reverts commit ca4a72779e13f9786c794efe556e2f0171433fcb.

--- bindings/node/scripts/neon-build.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-const { spawnSync } = require('child_process')
-
-spawnSync(
-  path.join(__dirname, '../node_modules/.bin/neon'),
-  ['build', '--release'],
-	{ stdio: 'inherit' }
-)
