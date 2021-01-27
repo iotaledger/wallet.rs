80416f88a23aebfd7ca4e05e5cfd0da029db2e42
Rajiv Shah <rajivshah1@icloud.com>
2021-01-27 13:03:20 -0500

    fix(ci): Use a matrix to build for each Node.js version

--- bindings/nodejs/scripts/node-neon-build.js
+++ bindings/nodejs/scripts/node-neon-build.js
@@ -3,4 +3,4 @@ const { spawnSync } = require('child_process');
 
 // Passing "--prepack 'yarn build:neon'" causes problems on Windows, so this is a workaround
 
-spawnSync('yarn', ['build:neon'], { stdio: 'inherit', cwd: resolve(__dirname, '../') });
+spawnSync(process.platform === 'win32' ? 'yarn.cmd' : 'yarn', ['build:neon'], { stdio: 'inherit', cwd: resolve(__dirname, '../') });
