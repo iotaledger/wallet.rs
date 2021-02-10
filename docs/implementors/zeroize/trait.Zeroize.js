b8c104fd644fbcaa9e85478e014892c33e85d689
lucasfernog <lucasfernog@users.noreply.github.com>
2021-02-10 18:02:35 +0000

    deploy: 4d0de7f5cd0a5012d399f0a436f40cf8cf6f5508

---
+++ docs/implementors/zeroize/trait.Zeroize.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["iota_wallet"] = [{"text":"impl <a class=\"trait\" href=\"https://docs.rs/zeroize/1.2.0/zeroize/trait.Zeroize.html\" title=\"trait zeroize::Zeroize\">Zeroize</a> for <a class=\"struct\" href=\"iota_wallet/stronghold/struct.Password.html\" title=\"struct iota_wallet::stronghold::Password\">Password</a>","synthetic":false,"types":["iota_wallet::stronghold::Password"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
