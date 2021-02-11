b9d59f5523f6103881b3a2f883c73d19465f8d0e
huhn511 <sehe89@gmail.com>
2021-02-11 20:05:17 +0100

    refactored "create-account" example

--- bindings/nodejs/examples/1-create-account.js
+++ bindings/nodejs/examples/1-create-account.js
@@ -1,37 +1,22 @@
-require('dotenv').config();
-
 /**
  * This example creates a new database and account
  * Please make sure you have the .env variables configured
  */
 
 async function run() {
-    const { AccountManager, StorageType, SignerType } = require('iota-wallet')
+    const { AccountManager, SignerType } = require('@iota/wallet')
     const manager = new AccountManager({
-        storagePath: './test-database',
-        storageType: StorageType.Sqlite
+        storagePath: './alice-database',
     })
-    
-    const mnemonic = process.env.IOTA_WALLET_MNEMONIC
-    
+    manager.setStrongholdPassword("your_password")
+    manager.storeMnemonic(SignerType.Stronghold, manager.generateMnemonic())
+
     const account = await manager.createAccount({
-      mnemonic,
-      alias: 'Test',
-      clientOptions: { node: process.env.NODE_URL, localPow: false },
-      signerType: SignerType.EnvMnemonic
+        clientOptions: { node: "https://api.lb-0.testnet.chrysalis2.com", localPow: true },
+        alias: 'Alice',
     })
 
-    console.log('alias', account.alias())
-    console.log('balance', account.balance())
-    
-    console.log('syncing account now')
-
-    const synced = await account.sync()
-
-    console.log('synced', synced)
-    console.log('acc messages', account.listMessages())
-    console.log('acc spent addresses', account.listAddresses(false))
-    console.log('acc unspent addresses', account.listAddresses(true))
+    console.log('Account created:', account.alias())
 }
 
 run()
