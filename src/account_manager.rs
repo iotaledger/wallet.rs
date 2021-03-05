e9fdebfe6092f810e15eb25396124a8f7722fa48
Lucas Nogueira <lucas@tauri.studio>
2021-03-05 00:17:59 -0300

    fix: tests

--- src/account_manager.rs
+++ src/account_manager.rs
@@ -1824,7 +1824,7 @@ mod tests {
                 BalanceChange::spent(3),
             ];
             for change in &change_events {
-                emit_balance_change(&account, account.latest_address().address(), vec![], change.clone())
+                emit_balance_change(&account, account.latest_address().address(), change.clone())
                     .await
                     .unwrap();
             }
