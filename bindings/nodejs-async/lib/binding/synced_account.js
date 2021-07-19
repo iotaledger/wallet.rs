8a7553053a03da15022d825b4a0c6e53a50bff95
melatron <antoni.dikov@gmail.com>
2021-07-19 11:06:23 +0300

    Synced account

--- bindings/nodejs-async/lib/binding/synced_account.js
+++ bindings/nodejs-async/lib/binding/synced_account.js
@@ -6,78 +6,12 @@ const utils = require('../utils.js');
 
 let {  } = addon;
 
-
-
-/*
-    id(): string;
-    index(): number;
-    alias(): string;
-    balance(): AccountBalance;
-    getNodeInfo(url?: string, auth?: Auth): Promise<NodeInfoWrapper>;
-    messageCount(messageType?: MessageType): number;
-    listMessages(count?: number, from?: number, messageType?: MessageType): Message[]
-    listAddresses(unspent?: boolean): Address[]
-    sync(options?: SyncOptions): Promise<SyncedAccount>
-    send(address: string, amount: number, options?: TransferOptions): Promise<Message>
-    sendToMany(outputs: TransferOutput[], options?: TransferOptions): Promise<Message>
-    retry(messageId: string): Promise<Message>
-    reattach(messageId: string): Promise<Message>
-    promote(messageId: string): Promise<Message>
-    consolidateOutputs(): Promise<Message[]>
-    setAlias(alias: string): void
-    setClientOptions(options: ClientOptions): void
-    getMessage(id: string): Message | undefined
-    getAddress(addressBech32: string): Address | undefined
-    generateAddress(): Address
-    generateAddresses(amount: number): Address[]
-    latestAddress(): Address
-    getUnusedAddress(): Address
-    isLatestAddressUnused(): Promise<boolean>
-*/
-
-
 class SyncedAccount {
-    constructor(account) {
+    constructor(id) {
       console.log("Synced account constructor called.");
-    //   this.accountId = accountId;
-      this.account = account;
-    }
-    async sync(options) {
-        if (options == undefined) {
-            return await syncAsync(this.account);
-        }
-
-        return await syncAsync(JSON.stringify(options), this.account);
-    }
-
-
-    async getNodeInfo(url) {
-        return await getNodeInfoAsync(url, this.account);
-    }
-
-    id() {
-        return id(this.account);
-    }
-
-    generateAddress() {
-        return generateAddress();
-    }
-
-    latestAddress() {
-        return latestAddress();
-    }
-
-    balance() {
-        return balance();
-    }
-
-    async send(transfer) {
-        return await sendAsync(JSON.stringify(transfer), this.account);
+      this.id = id;
     }
 
   };
 
-  module.exports.Account = Account;
-
-
-module.exports.Account = Account;
+module.exports.SyncedAccount = SyncedAccount;
