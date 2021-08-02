c32b8afad13e7b043619cbe598e9d2083651b634
Thoralf-M <thoralf.mue@gmail.com>
2021-08-02 16:43:57 +0200

    fix spent_addresses for no inputs

--- src/actor/message.rs
+++ src/actor/message.rs
@@ -529,7 +529,6 @@ pub struct MigrationDataDto {
 impl From<MigrationData> for MigrationDataDto {
     fn from(data: MigrationData) -> Self {
         let mut inputs: Vec<MigrationInputDto> = Vec::new();
-        let mut spent_addresses = false;
         for input in data.inputs {
             let address = input
                 .address
@@ -538,9 +537,6 @@ impl From<MigrationData> for MigrationDataDto {
                 .iter_trytes()
                 .map(char::from)
                 .collect::<String>();
-            if input.spent {
-                spent_addresses = true;
-            }
             inputs.push(MigrationInputDto {
                 address,
                 security_level: input.security_lvl,
@@ -554,7 +550,7 @@ impl From<MigrationData> for MigrationDataDto {
             balance: data.balance,
             last_checked_address_index: data.last_checked_address_index,
             inputs,
-            spent_addresses,
+            spent_addresses: data.spent_addresses,
         }
     }
 }
