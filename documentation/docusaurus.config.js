bdeea6220183af36f39412db4a32093a7415bfb1
lucas-tortora <85233773+lucas-tortora@users.noreply.github.com>
2021-08-03 11:05:16 +0200

    * Updated all docs images to use absolute paths instead of relative so they can be integrated into the upcoming wiki (#723)
    * Fixed broken links

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -47,7 +47,7 @@ module.exports = {
           items: [
             {
               label: 'Welcome',
-              to: '/docs/',
+              to: '/docs/welcome',
             },
             {
               label: 'Overview',
