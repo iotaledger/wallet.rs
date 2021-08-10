e0f0a47a43421054a97103ad4d4755b80235d8fa
melatron <antoni.dikov@gmail.com>
2021-08-10 16:01:54 +0300

    removed main.js

--- bindings/nodejs/lib/index.js
+++ bindings/nodejs/lib/index.js
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 const addon = require('../index.node');
-const types = require('./main.js')
 // const mh = require("./messageHandler.js");
 const el = require("./eventListener.js");
 // const am = require("./messages/accountManager.js");
