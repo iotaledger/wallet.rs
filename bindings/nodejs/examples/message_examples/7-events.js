e124317f356866d7701c973804855aa07665e7f4
Thoralf-M <thoralf.mue@gmail.com>
2021-08-30 13:50:36 +0200

    add event listener to account manager and add setClientOptions

--- bindings/nodejs/examples/message_examples/7-events.js
+++ bindings/nodejs/examples/message_examples/7-events.js
@@ -5,57 +5,60 @@
 require('dotenv').config();
 
 async function run() {
-  const {
-    AccountManagerForMessages,
-    EventListener,
-  } = require('../../lib/index.js');
+    const {
+        AccountManagerForMessages,
+    } = require('../../lib/index.js');
 
-  const manager = new AccountManagerForMessages({
-    storagePath: './alice-database',
-  });
+    const manager = new AccountManagerForMessages({
+        storagePath: './alice-database',
+    });
 
-  try {
-    await manager.setStrongholdPassword(process.env.SH_PASSWORD);
+    try {
+        await manager.setStrongholdPassword(process.env.SH_PASSWORD);
 
-    const account = await manager.getAccount('Alice');
-    console.log('Account:', account);
+        const account = await manager.getAccount('Alice');
+        console.log('Account:', account);
+        console.log(await account.alias());
+        await account.setClientOptions({
+            node: {
+                url: "https://api.hornet-3.testnet.chrysalis2.com/",
+            }
+        })
 
-    // Always sync before doing anything with the account
-    const synced = await account.sync();
-    console.log('Syncing... - ', synced);
+        // Always sync before doing anything with the account
+        const synced = await account.sync();
+        console.log('Syncing... - ', synced);
 
-    // You can also get the latest unused address:
-    const addressObject = await account.latestAddress();
-    console.log('Address:', addressObject);
+        // You can also get the latest unused address:
+        const addressObject = await account.latestAddress();
+        console.log('Address:', addressObject);
 
-    // Use the Chrysalis Faucet to send testnet tokens to your address:
-    console.log(
-      'Fill your address with the Faucet: https://faucet.testnet.chrysalis2.com/',
-    );
+        // Use the Chrysalis Faucet to send testnet tokens to your address:
+        console.log(
+            'Fill your address with the Faucet: https://faucet.testnet.chrysalis2.com/',
+        );
 
-    const callback = function (err, data) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('Data:', data);
-      }
-    };
+        const callback = function (err, data) {
+            console.log('data:', data);
+        };
+        manager.listen('BalanceChange', callback);
 
-    let el = new EventListener();
+        setTimeout(() => {
+            manager.removeEventListeners('BalanceChange');
+            console.log('event listeners removed');
+        }, 300000);
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
 
-    el.listen('BalanceChange', callback);
-  } catch (error) {
-    console.log('Error: ' + error);
-  }
-
-  // Possible Event Types:
-  //
-  // ErrorThrown
-  // BalanceChange
-  // NewTransaction
-  // ConfirmationStateChange
-  // Reattachment
-  // Broadcast
+    // Possible Event Types:
+    //
+    // ErrorThrown
+    // BalanceChange
+    // NewTransaction
+    // ConfirmationStateChange
+    // Reattachment
+    // Broadcast
 }
 
 run();
