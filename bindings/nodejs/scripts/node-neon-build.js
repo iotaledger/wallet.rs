be822879982bc024030ff123d43546176766b663
Rajiv Shah <rajivshah1@icloud.com>
2021-09-01 20:42:29 -0400

    fix(bindings/nodejs): Resolve issues with locations of prebuilt binaries

--- bindings/nodejs/scripts/node-neon-build.js
+++ bindings/nodejs/scripts/node-neon-build.js
@@ -1,5 +1,6 @@
 const { resolve } = require('path');
 const { spawnSync } = require('child_process');
+const moveArtifact = require('./move-artifact');
 
 // Passing "--prepack 'npm run build:neon'" causes problems on Windows, so this is a workaround
 
@@ -7,3 +8,5 @@ spawnSync('npm', ['run', 'build:neon'], {
     stdio: 'inherit',
     cwd: resolve(__dirname, '../'),
 });
+
+moveArtifact();
