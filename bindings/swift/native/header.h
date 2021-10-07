de0d7d92e9854a389169b2bbdb265af3235929d6
Pasquale Ambrosini <pasquale.ambrosini@gmail.com>
2021-10-07 13:47:11 +0200

    Updated header

--- bindings/swift/native/header.h
+++ bindings/swift/native/header.h
@@ -2,9 +2,9 @@ typedef void (*Callback)(const char *response);
 
 extern "C" {
 
-void initialize(Callback callback, const char *actor_id, const char *storage_path);
-void destroy(const char *actor_id);
-void send_message(const char *message);
-void listen(const char *actor_id, const char *id, const char *event_name);
+void iota_initialize(Callback callback, const char *actor_id, const char *storage_path);
+void iota_destroy(const char *actor_id);
+void iota_send_message(const char *message);
+void iota_listen(const char *actor_id, const char *id, const char *event_name);
 
 } // extern "C"
