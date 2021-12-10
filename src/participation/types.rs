db649360131914518d34b117fb44330253298261
Matthew Maxwell <maxwellmattryan@gmail.com>
2021-12-10 07:20:18 -0600

    chore: Update staking event IDs

--- src/participation/types.rs
+++ src/participation/types.rs
@@ -10,9 +10,9 @@ use std::{convert::TryInto, io::Read};
 /// Particiapation index
 pub(crate) const PARTICIPATE: &str = "PARTICIPATE";
 /// Event id for the shimmer staking
-pub(crate) const SHIMMER_EVENT_ID: &str = "c87b676749ad07819ba70ce1cee6349f3740a38bf94e0ae5c198b6870c683661";
+pub(crate) const SHIMMER_EVENT_ID: &str = "83ac8c9887b42bde8fefe729b5c98620f5cbfe757581589e4be993a11d3fcbba";
 /// Event id for the assembly staking
-pub(crate) const ASSEMBLY_EVENT_ID: &str = "7991ace8e7b98d8120777a5889e4474f9499fdad88b8b69241d054d49272f0c7";
+pub(crate) const ASSEMBLY_EVENT_ID: &str = "139333a7c3ed6314a7361537e720ed52ccf79ad2f794545bc1537018640b3ec7";
 
 /// Possible participation event types
 pub enum ParticipationEventType {
