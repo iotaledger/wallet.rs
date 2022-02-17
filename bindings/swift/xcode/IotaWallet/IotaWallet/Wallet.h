5e0ea510b97da3e871493a088fa3bf43a8d4f0e4
Oluwapelumi Ajayi <pelumi.ajayi@iota.org>
2022-02-17 11:36:49 +0100

    Add wrapper for ObjC. Add sample test in both ObjC and Swift.

---
+++ bindings/swift/xcode/IotaWallet/IotaWallet/Wallet.h
@@ -0,0 +1,17 @@
+
+#import <Foundation/Foundation.h>
+
+@interface Wallet : NSProxy
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void (^WalletHandler) (NSString * _Nullable message, NSError * _Nullable error);
+
+- (nullable instancetype) init;
+- (nullable instancetype) initWithStoragePath:(NSString *) path;
+- (void) sendMessage:(NSString *) message completion: (WalletHandler) completion;
+
+NS_ASSUME_NONNULL_END
+
+@end
+
---
+++ bindings/swift/xcode/IotaWallet/IotaWalletTests/callback.m
@@ -0,0 +1,37 @@
+#import <XCTest/XCTest.h>
+#import <IotaWallet/IotaWallet.h>
+
+@interface BlockCallback : XCTestCase
+
+@end
+
+@implementation BlockCallback
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testBlockCallback {
+    Wallet *wallet = [[Wallet alloc] init];
+    
+    NSString *message = @"{\"cmd\": \"CreateAccount\", \"payload\": { \"clientOptions\": { \"node\": \"https://nodes.devnet.iota.org:443\" } }, \"signerType\": { \"type\": \"Stronghold\" } }";
+    
+    XCTestExpectation *expectation = [self expectationWithDescription:@"CreateAccount"];
+    [wallet sendMessage:message completion:^(NSString * _Nullable message, NSError * _Nullable error) {
+        NSLog(@"-> %@", message);
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:2 handler:^(NSError * _Nullable error) {
+        if (error) {
+            XCTAssert(false, "Response timed out: %@", [error localizedDescription]);
+        }
+    }];
+    
+}
+
+@end
