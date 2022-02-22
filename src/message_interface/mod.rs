7d535ce3b3efa264a2873d317f99ae0827c5dfa6
Oluwapelumi Ajayi <pelumi.ajayi@iota.org>
2022-02-22 14:37:50 +0100

    Add manager options when creating message handler

--- src/message_interface/mod.rs
+++ src/message_interface/mod.rs
@@ -14,13 +14,38 @@ pub use message_handler::WalletMessageHandler;
 pub use message_type::{AccountToCreate, MessageType};
 pub use response::Response;
 pub use response_type::ResponseType;
+use serde::Deserialize;
 
-use crate::{account_manager::AccountManager, Result};
+use crate::{account_manager::AccountManager, client::ClientOptions, Result};
+use iota_client::signing::SignerHandle;
 use tokio::sync::mpsc::unbounded_channel;
 
-pub async fn create_message_handler(path: Option<String>) -> Result<WalletMessageHandler> {
-    let manager = if let Some(path) = path {
-        AccountManager::builder().with_storage_folder(&path).finish().await?
+#[derive(Deserialize)]
+pub struct ManagerOptions {
+    #[cfg(feature = "storage")]
+    storage_folder: Option<String>,
+    client_options: Option<String>,
+    signer: Option<String>,
+}
+
+pub async fn create_message_handler(options: Option<ManagerOptions>) -> Result<WalletMessageHandler> {
+    let manager = if let Some(options) = options {
+        let mut builder = AccountManager::builder();
+
+        #[cfg(feature = "storage")]
+        if let Some(storage_folder) = options.storage_folder {
+            builder = builder.with_storage_folder(&storage_folder);
+        }
+
+        if let Some(signer) = options.signer {
+            builder = builder.with_signer(SignerHandle::from_str(&signer)?);
+        }
+
+        if let Some(client_options) = options.client_options {
+            builder = builder.with_client_options(ClientOptions::new().from_json(&client_options)?);
+        }
+
+        builder.finish().await?
     } else {
         AccountManager::builder().finish().await?
     };
@@ -37,11 +62,40 @@ pub async fn send_message(handle: &WalletMessageHandler, message_type: MessageTy
 
 #[cfg(test)]
 mod tests {
-    use super::{AccountToCreate, MessageType, ResponseType};
+    use super::{AccountToCreate, MessageType, ResponseType, ManagerOptions};
 
     #[tokio::test]
     async fn create_account() {
-        let wallet_handle = super::create_message_handler(None).await.unwrap();
+        let signer = r#"{"Mnemonic":"acoustic trophy damage hint search taste love bicycle foster cradle brown govern endless depend situate athlete pudding blame question genius transfer van random vast"}"#.to_string();
+        let client_options = r#"{
+            "nodes":[
+               {
+                  "url":"http://localhost:14265/",
+                  "auth":null,
+                  "disabled":false
+               },
+               {
+                  "url":"https://chrysalis-nodes.iota.cafe/",
+                  "auth":null,
+                  "disabled":false
+               }
+            ],
+            "localPow":true,
+            "apiTimeout":{
+               "secs":20,
+               "nanos":0
+            }
+         }"#.to_string();
+
+         let options = ManagerOptions {
+            #[cfg(feature = "storage")]
+            storage_folder: Some("teststorage".to_string()),
+            client_options: Some(client_options),
+            signer: Some(signer),
+         };
+
+
+        let wallet_handle = super::create_message_handler(Some(options)).await.unwrap();
 
         // create an account
         let account = AccountToCreate { alias: None };
