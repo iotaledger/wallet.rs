497a1b05346a4b0d92998782304116460ef4ecfc
Oluwapelumi Ajayi <pelumi.ajayi@iota.org>
2022-02-28 15:29:27 +0100

    Update Swift binding

--- bindings/swift/xcode/IotaWallet/IotaWalletTests/await.swift
+++ bindings/swift/xcode/IotaWallet/IotaWalletTests/await.swift
@@ -7,6 +7,12 @@ import XCTest
 @testable import IotaWallet
 
 class SwiftAwait: XCTestCase {
+    
+    struct ManagerOptions: Codable {
+        var storageFolder: String?
+        var clientOptions: String?
+        var signer: String?
+    }
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,12 +22,59 @@ class SwiftAwait: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testAsyncAwait() async throws {
-        let wallet = IotaWallet.Wallet()!
+    func testCreateAccount() async throws {
+        let wallet = try! IotaWallet.Wallet()
+        
         let request = "{\"cmd\": \"CreateAccount\", \"payload\": { \"clientOptions\": { \"node\": \"https://nodes.devnet.iota.org:443\" } }, \"signerType\": { \"type\": \"Stronghold\" } }";
         
         let response = try! await wallet.sendMessage(request)
-        print("\(#function) -> \(response)")
+        print("\(response)")
+    }
+    
+    func testManagerOptions() async throws {
+        let signer = #"{"Mnemonic":"acoustic trophy damage hint search taste love bicycle foster cradle brown govern endless depend situate athlete pudding blame question genius transfer van random vast"}"#
+        let client_options = """
+        {
+            "nodes":[
+                {
+                    "url":"https://localhost",
+                    "auth":null,
+                    "disabled":false
+                }
+            ],
+            "localPow":true,
+            "apiTimeout":{
+                "secs":20,
+                "nanos":0
+            }
+        }
+        """
+        
+        let manager_options = ManagerOptions(storageFolder: "teststorage", clientOptions: client_options, signer: signer)
+        
+        let json = try JSONEncoder().encode(manager_options)
+        let manager_options_json = String(data: json, encoding: .utf8)!
+        
+        let _ = try! IotaWallet.Wallet(managerOptions: manager_options_json)
+    }
+    
+    func testEvents() async throws {
+        let wallet = try! IotaWallet.Wallet()
+        let expectation = self.expectation(description: "TestEvents")
+
+        try! wallet.listen([]) { (message: String?, error: Error?) in
+            print("Event received: \(message!)")
+            expectation.fulfill()
+        }
+        
+        // TODO: Create account and do something that would trigger an event
+        
+        await self.waitForExpectations(timeout: 2) { error in
+            if let err = error {
+                print("\(err)")
+            }
+        }
+        
     }
 
 }
