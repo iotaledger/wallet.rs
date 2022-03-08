307357b2730ea4d64a54232027c2ded8b1ad5bf9
Bing-Yang <51323441+bingyanglin@users.noreply.github.com>
2022-03-08 23:59:04 +0800

    feat (Python binding): Add necessary functions (create_message_handler, send_message, listen) (#905)
    * Init pythong binding
    
    * Add listen() binding
    
    * Fix typo
    
    * Error -> Wrong
    
    * Fix typo

--- bindings/python/native/src/types/error.rs
+++ bindings/python/native/src/types/error.rs
@@ -1,7 +1,6 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_client::bee_message;
 use iota_wallet::Error as RustError;
 use pyo3::{exceptions, prelude::*};
 use std::convert::From;
@@ -13,7 +12,7 @@ pub struct Error {
     pub error: PyErr,
 }
 
-impl std::convert::From<RustError> for Error {
+impl From<RustError> for Error {
     fn from(err: RustError) -> Self {
         Error {
             error: PyErr::new::<exceptions::PyValueError, _>(err.to_string()),
@@ -21,7 +20,15 @@ impl std::convert::From<RustError> for Error {
     }
 }
 
-impl std::convert::From<Error> for PyErr {
+impl From<serde_json::Error> for Error {
+    fn from(err: serde_json::Error) -> Self {
+        Error {
+            error: PyErr::new::<exceptions::PyValueError, _>(err.to_string()),
+        }
+    }
+}
+
+impl From<Error> for PyErr {
     fn from(err: Error) -> Self {
         err.error
     }
@@ -31,30 +38,6 @@ impl From<std::io::Error> for Error {
     fn from(err: std::io::Error) -> Self {
         Error {
             error: PyErr::new::<exceptions::PyIOError, _>(err.to_string()),
-        }
-    }
-}
-
-impl From<bee_message::Error> for Error {
-    fn from(err: bee_message::Error) -> Self {
-        Error {
-            error: PyErr::new::<exceptions::PyValueError, _>(err.to_string()),
-        }
-    }
-}
-
-impl From<hex::FromHexError> for Error {
-    fn from(err: hex::FromHexError) -> Self {
-        Error {
-            error: PyErr::new::<exceptions::PyValueError, _>(err.to_string()),
-        }
-    }
-}
-
-impl From<std::array::TryFromSliceError> for Error {
-    fn from(err: std::array::TryFromSliceError) -> Self {
-        Error {
-            error: PyErr::new::<exceptions::PyTypeError, _>(err.to_string()),
         }
     }
 }
--- bindings/python/native/src/types/mod.rs
+++ bindings/python/native/src/types/mod.rs
@@ -1,17 +1,7 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-mod account;
-mod address;
 mod error;
-mod event;
-mod message;
-mod migration;
-mod option;
-pub use account::*;
-pub use address::*;
+mod handler;
 pub use error::*;
-pub use event::*;
-pub use message::*;
-pub use migration::*;
-pub use option::*;
+pub use handler::*;
