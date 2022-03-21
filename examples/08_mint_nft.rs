6d8aa1b676674c980c6a266dba717296c3ba6d4a
Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
2022-03-21 12:59:07 +0100

    mint_nfts with minimum required amount and add mint_nfts example (#945)

---
+++ examples/08_mint_nft.rs
@@ -0,0 +1,43 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+//! cargo run --example 08_mint_nft --release
+// In this example we will mint a native token
+// Rename `.env.example` to `.env` first
+
+use dotenv::dotenv;
+use iota_wallet::{account_manager::AccountManager, signing::stronghold::StrongholdSigner, NftOptions, Result};
+
+use std::{env, path::Path};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // This example uses dotenv, which is not safe for use in production
+    dotenv().ok();
+    // Setup Stronghold signer
+    let signer = StrongholdSigner::try_new_signer_handle(
+        &env::var("STRONGHOLD_PASSWORD").unwrap(),
+        &Path::new("wallet.stronghold"),
+    )
+    .unwrap();
+
+    // Create the account manager
+    let manager = AccountManager::builder(signer).finish().await?;
+
+    // Get the account we generated with `01_create_wallet`
+    let account = manager.get_account("Alice").await?;
+
+    let nft_options = vec![NftOptions {
+        address: Some("atoi1qpszqzadsym6wpppd6z037dvlejmjuke7s24hm95s9fg9vpua7vluehe53e".to_string()),
+        immutable_metadata: Some(b"some immutable nft metadata".to_vec()),
+        metadata: Some(b"some nft metadata".to_vec()),
+    }];
+
+    let transfer_result = account.mint_nfts(nft_options, None).await?;
+    println!(
+        "Transaction: {} Message sent: http://localhost:14265/api/v2/messages/{}",
+        transfer_result.transaction_id,
+        transfer_result.message_id.expect("No message created yet")
+    );
+    Ok(())
+}
