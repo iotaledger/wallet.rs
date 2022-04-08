54ac3c2bc37ce1be43ccdd5599b30c52d0fe46ad
Umair Sarfraz <aquadestructor@icloud.com>
2022-04-09 00:45:39 +0200

    feat: some minor changes

--- bindings/nodejs/lib/accountManager.ts
+++ bindings/nodejs/lib/accountManager.ts
@@ -8,7 +8,7 @@ import EventListener from './eventListener';
 
 import type { IAccountManagerOptions } from './types'
 
-export default class AccountManagerForMessages {
+export class AccountManagerForMessages {
     messageHandler: MessageHandler;
     eventListener: EventListener | null;
 
