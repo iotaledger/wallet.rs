51070c7f05d4431177e7e9e3c8995c99057b03e2
Umair Sarfraz <aquadestructor@icloud.com>
2022-04-11 10:18:45 +0200

    feat: migrate to TS

--- bindings/nodejs/lib/messageHandler.ts
+++ bindings/nodejs/lib/messageHandler.ts
@@ -1,23 +1,50 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { sendMessage, messageHandlerNew } from './bindings';
-import { promisify } from './utils';
+import { sendMessageAsync, messageHandlerNew, listen } from './bindings';
+import type { EventType, AccountManagerOptions, __SendMessagePayload__ } from './types';
 
-console.log('Promiszify', promisify);
-console.log('Messate', messageHandlerNew);
-
-const sendMessageAsync = promisify(sendMessage);
-
+/**
+ * Message Handler class
+ */
 export default class MessageHandler {
     messageHandler: any
 
-    constructor(options: any) {
-        this.messageHandler = messageHandlerNew(JSON.stringify(options));
+    /**
+    * Creates a new instance of Account Manager
+    * 
+    * @param {AccountManagerOptions} options 
+    */
+    constructor(options: AccountManagerOptions) {
+        const messageOptions = {
+            storagePath: options?.storagePath,
+            clientOptions: JSON.stringify(options?.clientOptions),
+            signer: JSON.stringify(options?.signer)
+        }
+
+        this.messageHandler = messageHandlerNew(JSON.stringify(messageOptions));
     }
 
-    async sendMessage(message: unknown): Promise<string> {
-        // @ts-ignore
+    /**
+     * Sends a message to bindings
+     * 
+     * @param {Mess} message 
+     * 
+     * @returns {Promise<string>}
+     */
+    async sendMessage(message: __SendMessagePayload__): Promise<string> {
         return sendMessageAsync(JSON.stringify(message), this.messageHandler);
+    }
+
+    /**
+     * Listen to events supported by bindings
+     * 
+     * @param {EventType[]} eventTypes 
+     * @param {Function} callback 
+     * 
+     * @returns {void}
+     */
+    listen(eventTypes: EventType[], callback: (error: Error, result: string) => void): void {
+        return listen(eventTypes, callback, this.messageHandler);
     }
 }
