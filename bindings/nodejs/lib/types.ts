8422b74f3a92295dbe917ba73fc5ae1839b83623
Umair Sarfraz <aquadestructor@icloud.com>
2022-04-12 11:19:47 +0200

    feat: add listOutputs() method to Account

--- bindings/nodejs/lib/types.ts
+++ bindings/nodejs/lib/types.ts
@@ -11,6 +11,36 @@ export enum OutputsToCollect {
     All = 'All',
 }
 
+export interface OutputResponse {
+    messageId: string;
+    transactionId: string;
+    outputIndex: number;
+    isSpent: boolean;
+    milestoneIndexBooked: number;
+    milestoneTimestampBooked: number;
+    ledgerIndex: number;
+    // TODO: Replace with proper type
+    output: any;
+}
+
+export interface OutputData {
+    outputId: string;
+    outputResponse: OutputResponse;
+    // TODO: Replace with proper type
+    output: any;
+    amount: number;
+    isSpent: boolean;
+    address: {
+        // TODO: Also add other types
+        type: 'Ed25519',
+        data: string;
+    },
+    networkId: number;
+    remainder: boolean;
+    // TODO: Replace with proper type
+    chain: any;
+}
+
 export type RemainderValueStrategy = {
     ChangeAddress: {
         strategy: 'ChangeAddress';
@@ -113,7 +143,7 @@ export interface MqttBrokerOptions {
     useWs?: boolean;
     port?: number;
     maxReconnectionAttempts?: number;
-  }
+}
 
 export type Node = {
     url: string;
@@ -283,6 +313,15 @@ type __ListAddressesWithBalancePayload__ = {
         }
     };
 }
+type __ListOutputsPayload__ = {
+    cmd: 'CallAccountMethod'
+    payload: {
+        account_id: number;
+        method: {
+            name: 'ListOutputs'
+        }
+    };
+}
 
 export type __SendMessagePayload__ =
     | __GetAccountsMessagePayload__
@@ -303,3 +342,4 @@ export type __SendMessagePayload__ =
     | __GetOutputsWithAdditionalUnlockConditionsPayload__
     | __ListAddressesPayload__
     | __ListAddressesWithBalancePayload__
+    | __ListOutputsPayload__
