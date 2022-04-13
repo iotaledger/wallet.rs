9eb61645c3c1406e537da206be39e6e142d2bd48
Umair Sarfraz <aquadestructor@icloud.com>
2022-04-13 10:14:43 +0200

    feat: remove send() method

--- bindings/nodejs/lib/Account.ts
+++ bindings/nodejs/lib/Account.ts
@@ -102,21 +102,6 @@ export class Account {
         return JSON.parse(response).payload;
     }
 
-    async listUnspentOutputs(): Promise<OutputData[]> {
-        const response = await this.messageHandler.sendMessage({
-            cmd: 'CallAccountMethod',
-            payload: {
-                // TODO: Change to camelCase
-                account_id: this.meta.index,
-                method: {
-                    name: 'ListUnspentOutputs'
-                },
-            },
-        })
-
-        return JSON.parse(response).payload;
-    }
-
     async listPendingTransactions(): Promise<Transaction[]> {
         const response = await this.messageHandler.callAccountMethod(
             this.meta.index, {
@@ -193,7 +178,6 @@ export class Account {
             this.meta.index,
             {
                 name: 'GetBalance',
-
             }
         )
 
