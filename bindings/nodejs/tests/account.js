bf9b9bd68c683d0c7769e025e4f86619a64803e0
Tuditi <daviddetroch@pm.me>
2022-04-22 11:25:21 +0200

    chore: ran prettier

--- bindings/nodejs/tests/account.js
+++ bindings/nodejs/tests/account.js
@@ -2,43 +2,43 @@
 // SPDX-License-Identifier: Apache-2.0
 
 async function run() {
-  try {
-    const fs = require('fs');
-    fs.rmdirSync('./alice-database', { recursive: true });
-  } catch (e) {
-    // ignore it
-  }
-  const { AccountManagerForMessages } = require('../lib');
+    try {
+        const fs = require('fs');
+        fs.rmdirSync('./alice-database', { recursive: true });
+    } catch (e) {
+        // ignore it
+    }
+    const { AccountManagerForMessages } = require('../lib');
 
-  const manager = new AccountManagerForMessages({
-    storagePath: './alice-database',
-  });
+    const manager = new AccountManagerForMessages({
+        storagePath: './alice-database',
+    });
 
-  const account = manager.createAccount({
-    ClientOptions: {
-      node: 'http://api.hornet-3.testnet.chrysalis2.com',
-      requestTimeout: {
-        secs: 5000,
-        nanos: 0,
-      },
-      apiTimeout: {
-        PostMessage: {
-          secs: 6000,
-          nanos: 0,
+    const account = manager.createAccount({
+        ClientOptions: {
+            node: 'http://api.hornet-3.testnet.chrysalis2.com',
+            requestTimeout: {
+                secs: 5000,
+                nanos: 0,
+            },
+            apiTimeout: {
+                PostMessage: {
+                    secs: 6000,
+                    nanos: 0,
+                },
+            },
         },
-      },
-    },
-    alias: 'Alice',
-  });
-  console.log('Account created:', account);
-  account.setAlias('new alias');
+        alias: 'Alice',
+    });
+    console.log('Account created:', account);
+    account.setAlias('new alias');
 
-  const savedAccount = manager.getAccount('new alias');
-  console.log(savedAccount);
+    const savedAccount = manager.getAccount('new alias');
+    console.log(savedAccount);
 
-  account.getNodeInfo().then((value) => {
-    console.log(value);
-  });
+    account.getNodeInfo().then((value) => {
+        console.log(value);
+    });
 }
 
 run();
