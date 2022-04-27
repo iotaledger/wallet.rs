f040c4f5897c5599f3cc5550f145d45a64cff494
oddgrd <oddgrd@gmail.com>
2022-04-27 16:28:23 +0200

    Configure ESLint and Prettier

--- bindings/nodejs/.eslintrc.js
+++ bindings/nodejs/.eslintrc.js
@@ -1,13 +1,23 @@
 module.exports = {
     env: {
-        node: true,
-        browser: true,
         commonjs: true,
-        es2021: true,
+        es2019: true,
     },
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    plugins: ['@typescript-eslint'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier',
+    ],
+    parser: '@typescript-eslint/parser',
     parserOptions: {
         ecmaVersion: 12,
+        sourceType: 'module',
     },
-    rules: {},
+    rules: {
+        '@typescript-eslint/ban-ts-comment': [
+            'error',
+            { 'ts-ignore': 'allow-with-description' },
+        ],
+    },
 };
