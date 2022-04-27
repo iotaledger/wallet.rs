265cfe33fe8cc17f5039a903a95554183ed6e32e
Thoralf Müller <thoralf.mue@gmail.com>
2022-04-27 14:56:51 +0200

    fmt

--- src/account/operations/transfer/sign_transaction.rs
+++ src/account/operations/transfer/sign_transaction.rs
@@ -14,8 +14,6 @@ use iota_client::{
     constants::{IOTA_BECH32_HRP, SHIMMER_BECH32_HRP},
     secret::{types::InputSigningData, Network, SecretManageExt, SignMessageMetadata},
 };
-#[cfg(feature = "stronghold")]
-use iota_client::secret::SecretManager;
 
 use crate::account::{handle::AccountHandle, operations::transfer::TransactionPayload};
 #[cfg(feature = "events")]
