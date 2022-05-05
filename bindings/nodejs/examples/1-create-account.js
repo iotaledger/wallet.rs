f9b346f2268bb9a9c1d042e67f07fcf93d829ba9
Tuditi <daviddetroch@pm.me>
2022-05-05 09:59:03 +0200

    fix: fixed example + added coinType

--- bindings/nodejs/examples/1-create-account.js
+++ bindings/nodejs/examples/1-create-account.js
@@ -3,42 +3,17 @@
  */
 
 require('dotenv').config();
+const { CoinType } = require('../out/types');
 const manager = require('./account-manager');
 
 async function run() {
-    const { initLogger } = require('@iota/wallet');
-
-    initLogger({
-        color_enabled: true,
-        outputs: [{
-            name: './wallet.log',
-            level_filter: 'debug'
-        }]
-    })
-    
-    const manager = new AccountManager({
-        storagePath: './alice-database',
-        clientOptions: {
-            "nodes":[
-               {
-                  "url":"http://localhost:14265/",
-                  "auth":null,
-                  "disabled":false
-               }
-            ],
-            "localPow":true,
-         },
-        secretManager:{
-            "Mnemonic":"acoustic trophy damage hint search taste love bicycle foster cradle brown govern endless depend situate athlete pudding blame question genius transfer van random vast"
-        }
-    });
-
     try {
         // await manager.setStrongholdPassword(process.env.SH_PASSWORD);
         // await manager.storeMnemonic();
 
         const account = await manager.createAccount({
             alias: 'Alice',
+            coinType: CoinType.Shimmer,
         });
         console.log('Account created:', account);
     } catch (error) {
