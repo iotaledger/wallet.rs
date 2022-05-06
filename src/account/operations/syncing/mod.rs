591cf3886ebcac3235d2b06dabeddf745ac9c33c
Thoralf Müller <thoralf.mue@gmail.com>
2022-05-06 11:26:10 +0200

    Fix deadlock

--- src/account/operations/syncing/mod.rs
+++ src/account/operations/syncing/mod.rs
@@ -77,26 +77,22 @@ impl AccountHandle {
         let (spent_output_responses, _already_known_balance, _loaded_output_responses) =
             self.get_outputs(spent_output_ids.clone(), true).await?;
 
-        if options.automatic_output_consolidation {
-            // Only consolidates outputs for non ledger accounts, because they require approval from the user
-            match *self.secret_manager.read().await {
-                #[cfg(feature = "ledger-nano")]
-                // don't automatically consolidate with ledger accounts, because they require approval from the user
-                SecretManager::LedgerNano(_) | SecretManager::LedgerNanoSimulator(_) => {}
-                _ => {
-                    self.consolidate_outputs().await?;
-                }
-            }
+        let non_ledger_secret_manager = match *self.secret_manager.read().await {
+            #[cfg(feature = "ledger-nano")]
+            // don't automatically consolidate/collect outputs with ledger secret_managers, because they require
+            // approval from the user
+            SecretManager::LedgerNano(_) | SecretManager::LedgerNanoSimulator(_) => false,
+            _ => true,
+        };
+
+        // Only consolidates outputs for non ledger accounts, because they require approval from the user
+        if options.automatic_output_consolidation && non_ledger_secret_manager {
+            self.consolidate_outputs().await?;
         }
 
         // Only consolidates outputs for non ledger accounts, because they require approval from the user
-        match *self.secret_manager.read().await {
-            #[cfg(feature = "ledger-nano")]
-            // don't automatically consolidate with ledger accounts, because they require approval from the user
-            SecretManager::LedgerNano(_) | SecretManager::LedgerNanoSimulator(_) => {}
-            _ => {
-                self.try_collect_outputs(options.try_collect_outputs).await?;
-            }
+        if non_ledger_secret_manager {
+            self.try_collect_outputs(options.try_collect_outputs).await?;
         }
 
         // add a field to the sync options to also sync incoming transactions?
