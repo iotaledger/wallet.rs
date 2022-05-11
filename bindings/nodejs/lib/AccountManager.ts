aa552caff8f877b1b860261d9aa475e5e1422451
Thoralf Müller <thoralf.mue@gmail.com>
2022-05-11 11:02:33 +0200

    Update GetNodeInfo to also accept a provided url

--- bindings/nodejs/lib/AccountManager.ts
+++ bindings/nodejs/lib/AccountManager.ts
@@ -49,10 +49,11 @@ export class AccountManager {
         return accounts;
     }
 
-    async getNodeInfo(): Promise<NodeInfo> {
+    async getNodeInfo(url?: string, auth?: any): Promise<NodeInfo> {
         return JSON.parse(
             await this.messageHandler.sendMessage({
                     cmd: 'GetNodeInfo',
+                    payload: {url, auth}
                 }
             )
         );
