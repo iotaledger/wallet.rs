32f39b35bae30f0269859ddcfea6c79e7005be79
Tuditi <daviddetroch@pm.me>
2022-05-13 16:49:12 +0200

    fix: updated ClientOptions

--- bindings/nodejs/types/network.ts
+++ bindings/nodejs/types/network.ts
@@ -5,13 +5,28 @@ export enum Network {
     Testnet,
 }
 
-export type Auth = {
+type Auth = {
     jwt?: string;
     username?: string;
     password?: string;
 };
 
-export interface MqttBrokerOptions {
+interface NetworkInfo {
+    network?: string;
+    networkId?: number;
+    bech32HRP: string;
+    minPoWScore: number;
+    localPow: boolean;
+    fallbackToLocalPow: boolean;
+    tipsInterval: number;
+    rentStructure: {
+        vByteCost: number;
+        vByteFactorKey: number;
+        vByteFactorData: number;
+    }
+}
+
+interface MqttBrokerOptions {
     automaticDisconnect?: boolean;
     // timeout in seconds
     timeout?: number;
@@ -26,16 +41,27 @@ export type Node = {
     disabled?: boolean;
 };
 
-export interface ClientOptions {
+interface NodeManagerBuilder {
     primaryNode?: string | Node;
     primaryPoWNode?: string | Node;
-    node?: string | Node;
     nodes?: Array<string | Node>;
+    permanodes?: Array<string | Node>;
     network?: string;
-    mqttBrokerOptions?: MqttBrokerOptions;
-    quorumSize?: number;
-    quorumThreshold?: number;
-    localPow?: boolean;
+    nodeSyncEnabled: boolean;
+    nodeSyncInterval: number;
+    quorum: boolean;
+    minQuorumSize: number;
+    quorumTreshold?: boolean;
+}
+
+export interface ClientOptions {
+    nodeManagerBuilder?: NodeManagerBuilder;
+    brokerOptions: MqttBrokerOptions;
+    networkInfo: NetworkInfo;
+    apiTimeout: number;
+    remotePowTimeout: number;
+    offline: boolean;
+    powWorkerCount: number;
 }
 
 /**
