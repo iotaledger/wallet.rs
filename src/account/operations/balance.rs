24a13faee519809152c7e198c1675bb6b31ee2a8
pelumi-ajayi <95364138+pelumi-ajayi@users.noreply.github.com>
2022-05-18 11:40:22 +0100

    Refactor balance check for alias and foundry output (#1068)

--- src/account/operations/balance.rs
+++ src/account/operations/balance.rs
@@ -34,114 +34,116 @@ impl AccountHandle {
 
         for output_data in account.unspent_outputs.values() {
             // Check if output is from the network we're currently connected to
-            if output_data.network_id == network_id {
-                // Add alias and foundry outputs here because they can't have a [`StorageDepositReturnUnlockCondition`]
-                // or time related unlock conditions
-                match &output_data.output {
-                    Output::Foundry(output) => foundries.push(output.id()),
-                    Output::Alias(output) => {
-                        let alias_id = output.alias_id().or_from_output_id(output_data.output_id);
-                        aliases.push(alias_id);
-                    }
-                    _ => {}
+            if output_data.network_id != network_id {
+                continue;
+            }
+
+            // Add alias and foundry outputs here because they can't have a [`StorageDepositReturnUnlockCondition`]
+            // or time related unlock conditions
+            match &output_data.output {
+                Output::Foundry(output) => foundries.push(output.id()),
+                Output::Alias(output) => {
+                    let alias_id = output.alias_id().or_from_output_id(output_data.output_id);
+                    aliases.push(alias_id);
                 }
-
-                // If there is only an [AddressUnlockCondition], then we can spend the output at any time without
-                // restrictions
-                if output_data
-                    .output
-                    .unlock_conditions()
-                    .expect("no unlock_conditions")
-                    .len()
-                    == 1
-                {
-                    // add nft_id for nft outputs
-                    if let Output::Nft(output) = &output_data.output {
-                        let nft_id = output.nft_id().or_from_output_id(output_data.output_id);
-                        nfts.push(nft_id);
-                    }
-
-                    // Add amount
-                    total_amount += output_data.output.amount();
-                    // Add storage deposit
-                    required_storage_deposit += &output_data.output.byte_cost(&byte_cost_config);
-                    // Add native tokens
-                    if let Some(native_tokens) = output_data.output.native_tokens() {
-                        for native_token in native_tokens.iter() {
-                            match total_native_tokens.entry(*native_token.token_id()) {
-                                Entry::Vacant(e) => {
-                                    e.insert(*native_token.amount());
-                                }
-                                Entry::Occupied(mut e) => {
-                                    *e.get_mut() += *native_token.amount();
-                                }
-                            }
+                _ => {
+                    // If there is only an [AddressUnlockCondition], then we can spend the output at any time without
+                    // restrictions
+                    if output_data
+                        .output
+                        .unlock_conditions()
+                        .expect("no unlock_conditions")
+                        .len()
+                        == 1
+                    {
+                        // add nft_id for nft outputs
+                        if let Output::Nft(output) = &output_data.output {
+                            let nft_id = output.nft_id().or_from_output_id(output_data.output_id);
+                            nfts.push(nft_id);
                         }
-                    }
-                } else {
-                    // if we have multiple unlock conditions for basic or nft outputs, then we might can't spend the
-                    // balance at the moment or in the future
 
-                    let output_can_be_unlocked_now =
-                        unlockable_outputs_with_multiple_unlock_conditions.contains(&output_data.output_id);
-                    if !output_can_be_unlocked_now {
-                        potentially_locked_outputs.insert(output_data.output_id, false);
-                    }
-
-                    // For outputs that are expired or have a timelock unlock condition, but no expiration unlock
-                    // condition and we then can unlock them, then they can never be not available for us anymore and
-                    // should be added to the balance
-                    if output_can_be_unlocked_now {
-                        // check if output can be unlocked always from now on, in that case it should be added to
-                        // the total amount
-                        let output_can_be_unlocked_now_and_in_future =
-                            crate::account::operations::helpers::time::can_output_be_unlocked_forever_from_now_on(
-                                // We use the addresses with unspent outputs, because other addresses of the
-                                // account without unspent outputs can't be related to this output
-                                &account.addresses_with_unspent_outputs,
-                                output_data,
-                                local_time as u32,
-                                milestone_index,
-                            );
-
-                        if output_can_be_unlocked_now_and_in_future {
-                            // If output has a StorageDepositReturnUnlockCondition, the amount of it should be
-                            // subtracted, because this part needs to be sent back
-                            let amount = if let Some(unlock_conditions) = output_data.output.unlock_conditions() {
-                                if let Some(UnlockCondition::StorageDepositReturn(sdr)) =
-                                    unlock_conditions.get(StorageDepositReturnUnlockCondition::KIND)
-                                {
-                                    output_data.output.amount() - sdr.amount()
-                                } else {
-                                    output_data.output.amount()
-                                }
-                            } else {
-                                output_data.output.amount()
-                            };
-
-                            // Add amount
-                            total_amount += amount;
-                            // Add storage deposit
-                            required_storage_deposit += output_data.output.byte_cost(&byte_cost_config);
-                            // Add native tokens
-                            if let Some(native_tokens) = output_data.output.native_tokens() {
-                                for native_token in native_tokens.iter() {
-                                    match total_native_tokens.entry(*native_token.token_id()) {
-                                        Entry::Vacant(e) => {
-                                            e.insert(*native_token.amount());
-                                        }
-                                        Entry::Occupied(mut e) => {
-                                            *e.get_mut() += *native_token.amount();
-                                        }
+                        // Add amount
+                        total_amount += output_data.output.amount();
+                        // Add storage deposit
+                        required_storage_deposit += &output_data.output.byte_cost(&byte_cost_config);
+                        // Add native tokens
+                        if let Some(native_tokens) = output_data.output.native_tokens() {
+                            for native_token in native_tokens.iter() {
+                                match total_native_tokens.entry(*native_token.token_id()) {
+                                    Entry::Vacant(e) => {
+                                        e.insert(*native_token.amount());
+                                    }
+                                    Entry::Occupied(mut e) => {
+                                        *e.get_mut() += *native_token.amount();
                                     }
                                 }
                             }
-                        } else {
-                            // only add outputs that can't be locked now and at any point in the future
-                            potentially_locked_outputs.insert(output_data.output_id, true);
                         }
                     } else {
-                        potentially_locked_outputs.insert(output_data.output_id, false);
+                        // if we have multiple unlock conditions for basic or nft outputs, then we might can't spend the
+                        // balance at the moment or in the future
+
+                        let output_can_be_unlocked_now =
+                            unlockable_outputs_with_multiple_unlock_conditions.contains(&output_data.output_id);
+                        if !output_can_be_unlocked_now {
+                            potentially_locked_outputs.insert(output_data.output_id, false);
+                        }
+
+                        // For outputs that are expired or have a timelock unlock condition, but no expiration unlock
+                        // condition and we then can unlock them, then they can never be not available for us anymore
+                        // and should be added to the balance
+                        if output_can_be_unlocked_now {
+                            // check if output can be unlocked always from now on, in that case it should be added to
+                            // the total amount
+                            let output_can_be_unlocked_now_and_in_future =
+                                crate::account::operations::helpers::time::can_output_be_unlocked_forever_from_now_on(
+                                    // We use the addresses with unspent outputs, because other addresses of the
+                                    // account without unspent outputs can't be related to this output
+                                    &account.addresses_with_unspent_outputs,
+                                    output_data,
+                                    local_time as u32,
+                                    milestone_index,
+                                );
+
+                            if output_can_be_unlocked_now_and_in_future {
+                                // If output has a StorageDepositReturnUnlockCondition, the amount of it should be
+                                // subtracted, because this part needs to be sent back
+                                let amount = if let Some(unlock_conditions) = output_data.output.unlock_conditions() {
+                                    if let Some(UnlockCondition::StorageDepositReturn(sdr)) =
+                                        unlock_conditions.get(StorageDepositReturnUnlockCondition::KIND)
+                                    {
+                                        output_data.output.amount() - sdr.amount()
+                                    } else {
+                                        output_data.output.amount()
+                                    }
+                                } else {
+                                    output_data.output.amount()
+                                };
+
+                                // Add amount
+                                total_amount += amount;
+                                // Add storage deposit
+                                required_storage_deposit += output_data.output.byte_cost(&byte_cost_config);
+                                // Add native tokens
+                                if let Some(native_tokens) = output_data.output.native_tokens() {
+                                    for native_token in native_tokens.iter() {
+                                        match total_native_tokens.entry(*native_token.token_id()) {
+                                            Entry::Vacant(e) => {
+                                                e.insert(*native_token.amount());
+                                            }
+                                            Entry::Occupied(mut e) => {
+                                                *e.get_mut() += *native_token.amount();
+                                            }
+                                        }
+                                    }
+                                }
+                            } else {
+                                // only add outputs that can't be locked now and at any point in the future
+                                potentially_locked_outputs.insert(output_data.output_id, true);
+                            }
+                        } else {
+                            potentially_locked_outputs.insert(output_data.output_id, false);
+                        }
                     }
                 }
             }
