38dad5a8b283ba7b4b1ef016dbbdc0de287dc30b
Tuditi <daviddetroch@pm.me>
2022-05-19 09:59:18 +0200

    chore: added listOutputs example

---
+++ bindings/nodejs/examples/12-list-outputs.js
@@ -0,0 +1,20 @@
+/**
+ * This example creates a new database and account
+ */
+const unlockAndReturnManager = require('./account-manager');
+
+async function run() {
+    try {
+        const manager = await unlockAndReturnManager();
+        const account = await manager.getAccount('Alice');
+        
+        await account.sync();
+        const outputs = await account.listOutputs()
+        console.log('Outputs:', outputs);
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
+    process.exit(0);
+}
+
+run();
