16463a6d7e40141faa03ae8d727b29b3cbe72fb8
Tuditi <daviddetroch@pm.me>
2022-05-25 17:25:01 +0200

    feat: added getTransaction example

---
+++ bindings/nodejs/examples/15-get-transaction.js
@@ -0,0 +1,19 @@
+/**
+ * This example generates a new address.
+ */
+const getUnlockedManager = require('./account-manager');
+
+async function run() {
+    try {
+        const manager = await getUnlockedManager();
+        const account = await manager.getAccount('0');
+        
+        const response = await account.getTransaction('0xafc1205d93655dc1f3561f57291c79a6a50d8f4a95d9328c601d8038c479ecd4')
+        console.log(response)
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
+    process.exit(0);
+}
+
+run();
