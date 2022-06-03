d09c046e10846f4c66f8b2e631ff136fdc487f02
samuel_rufi <samuel.rufinatscha@iota.org>
2022-06-03 14:57:39 +0200

    Add storage test

--- tests/accounts.rs
+++ tests/accounts.rs
@@ -44,64 +44,100 @@ async fn account_ordering() -> Result<()> {
 #[tokio::test]
 async fn remove_latest_account() -> Result<()> {
     std::fs::remove_dir_all("test-storage/remove_latest_account").unwrap_or(());
+
     let client_options = ClientOptions::new()
         .with_node("http://localhost:14265")?
         .with_node_sync_disabled();
 
-    // mnemonic without balance
+    let (third_account_index, forth_account_index) = {
+        // mnemonic without balance
+        let secret_manager = MnemonicSecretManager::try_from_mnemonic(
+            "inhale gorilla deny three celery song category owner lottery rent author wealth penalty crawl hobby obtain glad warm early rain clutch slab august bleak",
+        )?;
+
+        let mut manager = AccountManager::builder()
+            .with_secret_manager(SecretManager::Mnemonic(secret_manager))
+            .with_client_options(client_options.clone())
+            .with_storage_path("test-storage/remove_latest_account")
+            .finish()
+            .await?;
+
+        // create two accounts
+        let first_account = manager.create_account().finish().await?;
+        let second_account = manager.create_account().finish().await?;
+        assert!(manager.get_accounts().await.unwrap().len() == 2);
+
+        // remove `second_account`
+        let removed_account = manager
+            .remove_latest_account()
+            .await
+            .expect("cannot remove latest account")
+            .unwrap();
+
+        // check if the `second_account` was removed successfully
+        let accounts = manager.get_accounts().await.unwrap();
+        assert!(accounts.len() == 1);
+        assert_eq!(
+            *accounts.get(0).unwrap().read().await.index(),
+            *first_account.read().await.index()
+        );
+        assert_eq!(
+            *removed_account.read().await.index(),
+            *second_account.read().await.index()
+        );
+
+        // remove `first_account`
+        let removed_account = manager
+            .remove_latest_account()
+            .await
+            .expect("cannot remove latest account")
+            .unwrap();
+
+        // check if the `first_account` was removed successfully
+        let accounts = manager.get_accounts().await.unwrap();
+        assert!(accounts.is_empty());
+        assert_eq!(
+            *removed_account.read().await.index(),
+            *first_account.read().await.index()
+        );
+
+        // try remove another time, even if there is nothing to remove
+        let removed_account = manager
+            .remove_latest_account()
+            .await
+            .expect("cannot remove latest account");
+
+        assert!(removed_account.is_none());
+
+        // add two new accounts and return their index
+
+        let third_account = manager.create_account().finish().await?;
+        let fourth_account = manager.create_account().finish().await?;
+
+        assert_eq!(manager.get_accounts().await.unwrap().len(), 2);
+
+        let third_account_index = *third_account.read().await.index();
+        let fourth_account_index = *fourth_account.read().await.index();
+
+        (third_account_index, fourth_account_index)
+    };
+
     let secret_manager = MnemonicSecretManager::try_from_mnemonic(
         "inhale gorilla deny three celery song category owner lottery rent author wealth penalty crawl hobby obtain glad warm early rain clutch slab august bleak",
     )?;
 
-    let mut manager = AccountManager::builder()
+    let manager = AccountManager::builder()
         .with_secret_manager(SecretManager::Mnemonic(secret_manager))
-        .with_client_options(client_options)
+        .with_client_options(client_options.clone())
         .with_storage_path("test-storage/remove_latest_account")
         .finish()
         .await?;
 
-    // create two accounts
-    let first_account = manager.create_account().finish().await?;
-    let second_account = manager.create_account().finish().await?;
-
-    assert!(manager.get_accounts().await.unwrap().len() == 2);
-    // remove `second_account`
-    let removed_account = manager
-        .remove_latest_account()
-        .await
-        .expect("cannot remove latest account")
-        .unwrap();
     let accounts = manager.get_accounts().await.unwrap();
-    assert!(accounts.len() == 1);
-    assert_eq!(
-        *accounts.get(0).unwrap().read().await.index(),
-        *first_account.read().await.index()
-    );
-    assert_eq!(
-        *removed_account.read().await.index(),
-        *second_account.read().await.index()
-    );
 
-    // remove `first_account`
-    let removed_account = manager
-        .remove_latest_account()
-        .await
-        .expect("cannot remove latest account")
-        .unwrap();
-    let accounts = manager.get_accounts().await.unwrap();
-    assert!(accounts.is_empty());
-    assert_eq!(
-        *removed_account.read().await.index(),
-        *first_account.read().await.index()
-    );
-
-    // test remove with no accounts
-    let removed_account = manager
-        .remove_latest_account()
-        .await
-        .expect("cannot remove latest account");
-
-    assert!(removed_account.is_none());
+    assert!(accounts.len() == 2);
+    assert_eq!(*accounts.get(0).unwrap().read().await.index(), third_account_index);
+    assert_eq!(*accounts.get(1).unwrap().read().await.index(), forth_account_index);
 
     std::fs::remove_dir_all("test-storage/remove_latest_account").unwrap_or(());
     #[cfg(debug_assertions)]
