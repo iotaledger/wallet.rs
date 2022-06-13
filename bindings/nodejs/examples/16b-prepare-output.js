cc8cc34c9e7d97df168adebaa89f4b05fe4362a7
Tuditi <daviddetroch@pm.me>
2022-06-13 14:26:32 +0200

    feat: added example

---
+++ bindings/nodejs/examples/16b-prepare-output.js
@@ -0,0 +1,29 @@
+/**
+ * This example prepares a transaction, calculates it storage deposit and sends it.
+ */
+
+const getUnlockedManager = require('./account-manager');
+
+async function run() {
+    try {
+        const manager = await getUnlockedManager();
+        const account = await manager.getAccount('Alice');
+
+        //TODO: Replace with the address of your choice!
+        const recipientAddress =
+            'rms1qrrv7flg6lz5cssvzv2lsdt8c673khad060l4quev6q09tkm9mgtupgf0h0';
+        const amount = '1000';
+
+        const output = await account.prepareOutput({
+            recipientAddress,
+            amount
+        });
+
+        console.log(output);
+    } catch (error) {
+        console.log('Error: ' + error);
+    }
+    process.exit(0);
+}
+
+run();
