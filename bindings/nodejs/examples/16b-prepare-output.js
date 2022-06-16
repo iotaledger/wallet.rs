1502e975f5cb0bada896c3f4930b6bbf1d194ac5
Tuditi <daviddetroch@pm.me>
2022-06-16 17:33:24 +0200

    bug report: tag and metadata not shown as string

--- bindings/nodejs/examples/16b-prepare-output.js
+++ bindings/nodejs/examples/16b-prepare-output.js
@@ -8,22 +8,27 @@ async function run() {
     try {
         const manager = await getUnlockedManager();
         const account = await manager.getAccount('Alice');
-
-        //TODO: Replace with the address of your choice!
         const recipientAddress =
             'rms1qrrv7flg6lz5cssvzv2lsdt8c673khad060l4quev6q09tkm9mgtupgf0h0';
         const amount = '1000';
 
         const output = await account.prepareOutput({
             recipientAddress,
-            amount
-        });
-
-        console.log(output);
+            amount,
+            features:  {
+                tag: 'Random tag',
+                metadata: 'Random Data',
+            }
+        })
+        
+        const response = await account.sendOutputs([output])
+        const transaction = await account.getTransaction(response.transactionId)
+        const outputs = transaction.payload.essence.outputs[0]
+        console.log('Tag and metadata should show as string below:')
+        console.log(outputs)
     } catch (error) {
         console.log('Error: ' + error);
     }
-    process.exit(0);
 }
 
 run();
