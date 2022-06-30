de6ae844684c19c0c2cbb239fce9202772f18e7f
Thoralf Müller <thoralf.mue@gmail.com>
2022-06-30 18:00:09 +0200

    Add get_ledger_status and ledger_nano example

---
+++ src/account_manager/operations/ledger_nano.rs
@@ -0,0 +1,18 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+use iota_client::secret::{LedgerStatus, SecretManager};
+
+use crate::account_manager::AccountManager;
+
+impl AccountManager {
+    /// Get the ledger nano status
+    pub async fn get_ledger_status(&self) -> crate::Result<LedgerStatus> {
+        match &*self.secret_manager.read().await {
+            SecretManager::LedgerNano(stronghold) | SecretManager::LedgerNanoSimulator(stronghold) => {
+                Ok(stronghold.get_ledger_status().await)
+            }
+            _ => Err(iota_client::Error::SecretManagerMismatch.into()),
+        }
+    }
+}
