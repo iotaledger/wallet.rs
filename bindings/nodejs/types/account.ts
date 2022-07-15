8442dc2cce3d395127a279f48955f2ce0b9bc35a
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-07-15 15:14:19 +0200

    Rename `id` to `tokenId` for native tokens (#1298)

--- bindings/nodejs/types/account.ts
+++ bindings/nodejs/types/account.ts
@@ -72,7 +72,7 @@ export interface BaseCoinBalance {
 }
 
 export interface NativeTokenBalance {
-    id: string
+    tokenId: string
     total: HexEncodedAmount;
     available: HexEncodedAmount;
 }
