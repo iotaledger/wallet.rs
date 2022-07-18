39ef672bbfba5f4d4d5eb8cc4dcb268b803fb36b
Thoralf Müller <thoralf.mue@gmail.com>
2022-07-18 10:44:30 +0200

    Fix import

--- bindings/nodejs/lib/bindings.ts
+++ bindings/nodejs/lib/bindings.ts
@@ -3,14 +3,16 @@
 
 import type { MessageHandler } from './MessageHandler';
 // @ts-ignore: path is set to match runtime transpiled js path
-import {
-    initLogger as internalInitLogger,
+import addon = require('../../index.node');
+
+const {
+    initLogger,
     sendMessage,
     messageHandlerNew,
     listen,
     clearListeners,
     destroy,
-} from '../../index.node';
+} = addon;
 
 const sendMessageAsync = (
     message: string,
@@ -27,7 +29,7 @@ const sendMessageAsync = (
     });
 
 export {
-    internalInitLogger,
+    initLogger as internalInitLogger,
     sendMessageAsync,
     messageHandlerNew,
     listen,
