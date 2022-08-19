a6bbc715e638d59ff4b70da1dfc04d01a439f247
Tuditi <daviddetroch@pm.me>
2022-08-19 17:53:52 +0200

    fix: correct move of artifact for electron build

--- bindings/nodejs/scripts/electron-neon-build.js
+++ bindings/nodejs/scripts/electron-neon-build.js
@@ -1,6 +1,9 @@
 const { promisify } = require('util');
+const moveArtifact = require('./move-artifact');
 const build = promisify(require('electron-build-env'));
 
 build(['npm', 'run', 'build:neon'], {
     electron: process.env.CURRENT_ELECTRON_VERSION,
 }).then(() => process.exit(0));
+
+moveArtifact();
