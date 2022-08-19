9e69f104aa303c84adb7fff2e94172a5be51ea7e
Tuditi <daviddetroch@pm.me>
2022-08-19 17:38:05 +0200

    fix: correctly place index.node in build/Release

--- bindings/nodejs/lib/bindings.ts
+++ bindings/nodejs/lib/bindings.ts
@@ -3,7 +3,7 @@
 
 import type { MessageHandler } from './MessageHandler';
 // @ts-ignore: path is set to match runtime transpiled js path
-import addon = require('../../index.node');
+import addon = require('../../build/Release/index.node');
 
 const {
     initLogger,
--- bindings/nodejs/scripts/strip.js
+++ bindings/nodejs/scripts/strip.js
@@ -2,7 +2,8 @@ const { resolve } = require('path');
 const { spawnSync } = require('child_process');
 
 // Based on https://github.com/prebuild/prebuild/blob/master/strip.js
+// Prebuild requires that the binary is in `build/Release` as though it was built with node-gyp
 
-const binaryPath = resolve(__dirname, '../index.node');
+const binaryPath = resolve(__dirname, '../build/Release/index.node');
 const stripArgs = process.platform === 'darwin' ? '-Sx' : '--strip-all';
 spawnSync('strip', [stripArgs, binaryPath], { stdio: 'inherit' });
