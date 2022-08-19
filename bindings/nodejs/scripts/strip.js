7404b9a5f885116a6d476ea8ed56cc2df9c63c47
Mark Nardi <mark.nardi@iota.org>
2022-08-19 12:42:24 +0200

    fix: revert strip.js for locating index.node

--- bindings/nodejs/scripts/strip.js
+++ bindings/nodejs/scripts/strip.js
@@ -3,19 +3,6 @@ const { spawnSync } = require('child_process');
 
 // Based on https://github.com/prebuild/prebuild/blob/master/strip.js
 
-if (process.platform === 'win32') {
-    // strip doesn't exist on Windows
-    return;
-}
-
-const binaryPath = resolve(__dirname, '../build/Release/index.node');
+const binaryPath = resolve(__dirname, '../index.node');
 const stripArgs = process.platform === 'darwin' ? '-Sx' : '--strip-all';
-const { status } = spawnSync('strip', [stripArgs, binaryPath], {
-    stdio: 'inherit',
-});
-
-if (status === null) {
-    process.exit(1);
-} else if (status > 0) {
-    process.exit(status);
-}
+spawnSync('strip', [stripArgs, binaryPath], { stdio: 'inherit' });
