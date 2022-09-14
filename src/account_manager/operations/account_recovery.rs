7501f220952321ac88494b7bd8bee0e95b2753f6
Thoralf Müller <thoralf.mue@gmail.com>
2022-09-14 13:12:17 +0200

    Update gap limit logic for accounts

--- src/account_manager/operations/account_recovery.rs
+++ src/account_manager/operations/account_recovery.rs
@@ -111,12 +111,13 @@ impl AccountManager {
         max_account_index_to_keep: &mut Option<u32>,
         sync_options: Option<SyncOptions>,
     ) -> crate::Result<()> {
+        let mut updated_account_gap_limit = account_gap_limit;
         loop {
-            log::debug!("[recover_accounts] generating {account_gap_limit} new accounts");
+            log::debug!("[recover_accounts] generating {updated_account_gap_limit} new accounts");
 
             // Generate account with addresses and get their outputs in parallel
             let mut tasks = Vec::new();
-            for _ in 0..account_gap_limit {
+            for _ in 0..updated_account_gap_limit {
                 let mut new_account = self.create_account();
                 let sync_options_ = sync_options.clone();
                 tasks.push(async move {
@@ -133,12 +134,14 @@ impl AccountManager {
             }
 
             let results: Vec<crate::Result<(u32, usize)>> = futures::future::try_join_all(tasks).await?;
-            let mut total_account_outputs_count = 0;
+
+            let mut new_accounts_with_outputs = 0;
+            let mut highest_account_index = 0;
             for res in results {
                 let (account_index, outputs_count): (u32, usize) = res?;
-                total_account_outputs_count += outputs_count;
-
                 if outputs_count != 0 {
+                    new_accounts_with_outputs += 1;
+
                     match *max_account_index_to_keep {
                         Some(max_account_index) => {
                             if account_index > max_account_index {
@@ -148,11 +151,23 @@ impl AccountManager {
                         None => *max_account_index_to_keep = Some(account_index),
                     }
                 }
+
+                if account_index > highest_account_index {
+                    highest_account_index = account_index;
+                }
             }
 
-            // If all accounts in this round have no outputs, we break
-            if total_account_outputs_count == 0 {
+            // Break if there is no new account with outputs
+            if new_accounts_with_outputs == 0 {
                 break;
+            }
+
+            // Update account_gap_limit to only create so many new accounts, that we would check the initial provided
+            // account_gap_limit amount of empty accounts
+            if let Some(max_account_index_to_keep) = &max_account_index_to_keep {
+                let empty_accounts_in_row = highest_account_index - max_account_index_to_keep;
+                log::debug!("[recover_accounts] empty_accounts_in_row {empty_accounts_in_row}");
+                updated_account_gap_limit = account_gap_limit - empty_accounts_in_row;
             }
         }
 
