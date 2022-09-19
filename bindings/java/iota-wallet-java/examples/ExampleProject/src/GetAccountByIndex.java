45f05b77d55c6e6d44e57fe9ace8be9b5b99b5f8
samuel_rufi <samuel.rufinatscha@iota.org>
2022-09-19 17:47:30 +0200

    Add first examples

---
+++ bindings/java/iota-wallet-java/examples/ExampleProject/src/GetAccountByIndex.java
@@ -0,0 +1,32 @@
+import org.iota.Wallet;
+import org.iota.types.Account;
+import org.iota.types.ClientConfig;
+import org.iota.types.WalletConfig;
+import org.iota.types.exceptions.WalletException;
+import org.iota.types.ids.account.AccountAlias;
+import org.iota.types.ids.account.AccountIndex;
+import org.iota.types.secret.MnemonicSecretManager;
+
+public class GetAccountByIndex {
+    private static final String SHIMMER_TESTNET_NODE_URL = "https://api.testnet.shimmer.network";
+    private static final int SHIMMER_COIN_TYPE = 4219;
+    private static final String DEFAULT_DEVELOPMENT_MNEMONIC = "hidden enroll proud copper decide negative orient asset speed work dolphin atom unhappy game cannon scheme glow kid ring core name still twist actor";
+
+    public static void main(String[] args) throws WalletException {
+        // Build the wallet
+        Wallet wallet = new Wallet(new WalletConfig()
+                .withClientOptions(new ClientConfig().withNodes(new String[] { SHIMMER_TESTNET_NODE_URL }))
+                .withSecretManager(new MnemonicSecretManager(DEFAULT_DEVELOPMENT_MNEMONIC))
+                .withCoinType(SHIMMER_COIN_TYPE)
+        );
+
+        // Create an account
+        Account createdAccount = wallet.createAccount("Hans");
+
+        // Get the account by alias
+        Account fetched = wallet.getAccount(new AccountIndex(createdAccount.getIndex()));
+
+        // Print the account
+        System.out.println(fetched);
+    }
+}
