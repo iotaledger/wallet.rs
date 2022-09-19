b0b5b49eb294770ea88f571bebcbf00a0ea8de48
samuel_rufi <samuel.rufinatscha@iota.org>
2022-09-19 16:18:45 +0200

    Fix ordering of APIs

--- bindings/java/iota-wallet-java/src/main/java/org/iota/Wallet.java
+++ bindings/java/iota-wallet-java/src/main/java/org/iota/Wallet.java
@@ -23,6 +23,8 @@ public class Wallet extends NativeApi {
         super(config);
     }
 
+    // Account manager APIs
+
     public Account createAccount(String alias) throws WalletException {
         JsonObject o = new JsonObject();
         o.addProperty("alias", alias);
@@ -42,6 +44,120 @@ public class Wallet extends NativeApi {
             accounts[i] = GsonSingleton.getInstance().fromJson(responsePayload.get(i).getAsJsonObject(), Account.class);
 
         return accounts;
+    }
+
+
+    public void backup(String destination, String password) throws WalletException {
+        JsonObject o = new JsonObject();
+        o.addProperty("destination", destination);
+        o.addProperty("password", password);
+
+        callBaseApi(new ClientCommand("Backup", o));
+    }
+
+    public void changeStrongholdPassword(String currentPassword, String newPassword) throws WalletException {
+        JsonObject o = new JsonObject();
+        o.addProperty("currentPassword", currentPassword);
+        o.addProperty("newPassword", newPassword);
+
+        callBaseApi(new ClientCommand("ChangeStrongholdPassword", o));
+    }
+
+    public void clearStrongholdPassword() throws WalletException {
+        callBaseApi(new ClientCommand("ClearStrongholdPassword"));
+    }
+
+    public boolean isStrongholdPasswordAvailable() throws WalletException {
+        return callBaseApi(new ClientCommand("IsStrongholdPasswordAvailable")).getAsBoolean();
+    }
+
+    public void recoverAccounts(int accountGapLimit, int addressGapLimit, SyncOptions sync_options) throws WalletException {
+        JsonObject o = new JsonObject();
+        o.addProperty("accountGapLimit", accountGapLimit);
+        o.addProperty("addressGapLimit", addressGapLimit);
+        o.add("sync_options", GsonSingleton.getInstance().toJsonTree(sync_options));
+
+        callBaseApi(new ClientCommand("RecoverAccounts", o));
+    }
+
+    public void restoreBackup(String source, String password) throws WalletException {
+        JsonObject o = new JsonObject();
+        o.addProperty("source", source);
+        o.addProperty("password", password);
+
+        callBaseApi(new ClientCommand("RestoreBackup", o));
+    }
+
+    public void removeLatestAccount() throws WalletException {
+        callBaseApi(new ClientCommand("RemoveLatestAccount"));
+    }
+
+    public String generateMnemonic() throws WalletException {
+        return callBaseApi(new ClientCommand("GenerateMnemonic")).getAsString();
+    }
+
+    public String verifyMnemonic(String mnemonic) throws WalletException {
+        JsonPrimitive p = new JsonPrimitive(mnemonic);
+        return callBaseApi(new ClientCommand("VerifyMnemonic", p)).getAsString();
+    }
+
+    public String setClientOptions(ClientConfig config) throws WalletException {
+        return callBaseApi(new ClientCommand("SetClientOptions", GsonSingleton.getInstance().toJsonTree(config))).getAsString();
+    }
+
+    public LedgerNanoStatus getLedgerNanoStatus() throws WalletException {
+        return GsonSingleton.getInstance().fromJson(callBaseApi(new ClientCommand("GetLedgerNanoStatus")), LedgerNanoStatus.class);
+    }
+
+    public JsonObject getNodeInfo(String url, NodeAuth auth) throws WalletException {
+        JsonObject p = new JsonObject();
+        p.addProperty("url", url);
+        p.add("auth", GsonSingleton.getInstance().toJsonTree(auth));
+
+        return (JsonObject) callBaseApi(new ClientCommand("GetNodeInfo", p));
+    }
+
+    public void setStrongholdPassword(String password) throws WalletException {
+        JsonPrimitive p = new JsonPrimitive(password);
+        callBaseApi(new ClientCommand("SetStrongholdPassword", p));
+    }
+
+    public void setStrongholdPassword(int interval) throws WalletException {
+        JsonPrimitive p = new JsonPrimitive(interval);
+        callBaseApi(new ClientCommand("SetStrongholdPasswordClearInterval", p));
+    }
+
+    public void storeMnemonic(String mnemonic) throws WalletException {
+        JsonPrimitive p = new JsonPrimitive(mnemonic);
+        callBaseApi(new ClientCommand("StoreMnemonic", p));
+    }
+
+    public void startBackgroundSync(SyncOptions options, int intervalInMilliseconds) throws WalletException {
+        JsonObject o = new JsonObject();
+        o.add("options", GsonSingleton.getInstance().toJsonTree(options));
+        o.addProperty("intervalInMilliseconds", intervalInMilliseconds);
+
+        callBaseApi(new ClientCommand("StartBackgroundSync", o));
+    }
+
+    public void stopBackgroundSync(SyncOptions options, int intervalInMilliseconds) throws WalletException {
+        callBaseApi(new ClientCommand("StopBackgroundSync"));
+    }
+
+    public void emitTestEvent(JsonElement event) throws WalletException {
+        callBaseApi(new ClientCommand("EmitTestEvent", event));
+    }
+
+    public String bech32ToHex(String bech32) throws WalletException {
+        return callBaseApi(new ClientCommand("Bech32ToHex", new JsonPrimitive(bech32))).getAsString();
+    }
+
+    public String hexToBech32(String hex, String bech32Hrp) throws WalletException {
+        JsonObject p = new JsonObject();
+        p.addProperty("hex", hex);
+        p.addProperty("bech32Hrp", bech32Hrp);
+
+        return callBaseApi(new ClientCommand("HexToBech32", p)).getAsString();
     }
 
     // Account Method APIs
@@ -261,119 +377,6 @@ public class Wallet extends NativeApi {
 
     public TransactionPayload claimOutputs(AccountIdentifier accountIdentifier, ClaimOutputs method) throws WalletException {
         return GsonSingleton.getInstance().fromJson(callAccountMethod(accountIdentifier, method), TransactionPayload.class);
-    }
-
-    public void backup(String destination, String password) throws WalletException {
-        JsonObject o = new JsonObject();
-        o.addProperty("destination", destination);
-        o.addProperty("password", password);
-
-        callBaseApi(new ClientCommand("Backup", o));
-    }
-
-    public void changeStrongholdPassword(String currentPassword, String newPassword) throws WalletException {
-        JsonObject o = new JsonObject();
-        o.addProperty("currentPassword", currentPassword);
-        o.addProperty("newPassword", newPassword);
-
-        callBaseApi(new ClientCommand("ChangeStrongholdPassword", o));
-    }
-
-    public void clearStrongholdPassword() throws WalletException {
-        callBaseApi(new ClientCommand("ClearStrongholdPassword"));
-    }
-
-    public boolean isStrongholdPasswordAvailable() throws WalletException {
-        return callBaseApi(new ClientCommand("IsStrongholdPasswordAvailable")).getAsBoolean();
-    }
-
-    public void recoverAccounts(int accountGapLimit, int addressGapLimit, SyncOptions sync_options) throws WalletException {
-        JsonObject o = new JsonObject();
-        o.addProperty("accountGapLimit", accountGapLimit);
-        o.addProperty("addressGapLimit", addressGapLimit);
-        o.add("sync_options", GsonSingleton.getInstance().toJsonTree(sync_options));
-
-        callBaseApi(new ClientCommand("RecoverAccounts", o));
-    }
-
-    public void restoreBackup(String source, String password) throws WalletException {
-        JsonObject o = new JsonObject();
-        o.addProperty("source", source);
-        o.addProperty("password", password);
-
-        callBaseApi(new ClientCommand("RestoreBackup", o));
-    }
-
-    public void removeLatestAccount() throws WalletException {
-        callBaseApi(new ClientCommand("RemoveLatestAccount"));
-    }
-
-    public String generateMnemonic() throws WalletException {
-        return callBaseApi(new ClientCommand("GenerateMnemonic")).getAsString();
-    }
-
-    public String verifyMnemonic(String mnemonic) throws WalletException {
-        JsonPrimitive p = new JsonPrimitive(mnemonic);
-        return callBaseApi(new ClientCommand("VerifyMnemonic", p)).getAsString();
-    }
-
-    public String setClientOptions(ClientConfig config) throws WalletException {
-        return callBaseApi(new ClientCommand("SetClientOptions", GsonSingleton.getInstance().toJsonTree(config))).getAsString();
-    }
-
-    public LedgerNanoStatus getLedgerNanoStatus() throws WalletException {
-        return GsonSingleton.getInstance().fromJson(callBaseApi(new ClientCommand("GetLedgerNanoStatus")), LedgerNanoStatus.class);
-    }
-
-    public JsonObject getNodeInfo(String url, NodeAuth auth) throws WalletException {
-        JsonObject p = new JsonObject();
-        p.addProperty("url", url);
-        p.add("auth", GsonSingleton.getInstance().toJsonTree(auth));
-
-        return (JsonObject) callBaseApi(new ClientCommand("GetNodeInfo", p));
-    }
-
-    public void setStrongholdPassword(String password) throws WalletException {
-        JsonPrimitive p = new JsonPrimitive(password);
-        callBaseApi(new ClientCommand("SetStrongholdPassword", p));
-    }
-
-    public void setStrongholdPassword(int interval) throws WalletException {
-        JsonPrimitive p = new JsonPrimitive(interval);
-        callBaseApi(new ClientCommand("SetStrongholdPasswordClearInterval", p));
-    }
-
-    public void storeMnemonic(String mnemonic) throws WalletException {
-        JsonPrimitive p = new JsonPrimitive(mnemonic);
-        callBaseApi(new ClientCommand("StoreMnemonic", p));
-    }
-
-    public void startBackgroundSync(SyncOptions options, int intervalInMilliseconds) throws WalletException {
-        JsonObject o = new JsonObject();
-        o.add("options", GsonSingleton.getInstance().toJsonTree(options));
-        o.addProperty("intervalInMilliseconds", intervalInMilliseconds);
-
-        callBaseApi(new ClientCommand("StartBackgroundSync", o));
-    }
-
-    public void stopBackgroundSync(SyncOptions options, int intervalInMilliseconds) throws WalletException {
-        callBaseApi(new ClientCommand("StopBackgroundSync"));
-    }
-
-    public void emitTestEvent(JsonElement event) throws WalletException {
-        callBaseApi(new ClientCommand("EmitTestEvent", event));
-    }
-
-    public String bech32ToHex(String bech32) throws WalletException {
-        return callBaseApi(new ClientCommand("Bech32ToHex", new JsonPrimitive(bech32))).getAsString();
-    }
-
-    public String hexToBech32(String hex, String bech32Hrp) throws WalletException {
-        JsonObject p = new JsonObject();
-        p.addProperty("hex", hex);
-        p.addProperty("bech32Hrp", bech32Hrp);
-
-        return callBaseApi(new ClientCommand("HexToBech32", p)).getAsString();
     }
 
 }
