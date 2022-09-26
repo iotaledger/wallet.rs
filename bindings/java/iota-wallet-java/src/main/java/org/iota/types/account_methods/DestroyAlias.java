8fdbfb34d5abd34e7078f7fe9a87f55bc6edbdec
Samuel Rufinatscha <samuel.rufinatscha@gmail.com>
2022-09-26 10:16:27 +0200

    Update bindings/java/iota-wallet-java/src/main/java/org/iota/types/account_methods/DestroyAlias.java
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- bindings/java/iota-wallet-java/src/main/java/org/iota/types/account_methods/DestroyAlias.java
+++ bindings/java/iota-wallet-java/src/main/java/org/iota/types/account_methods/DestroyAlias.java
@@ -6,9 +6,7 @@ package org.iota.types.account_methods;
 import org.iota.types.TransactionOptions;
 import org.iota.types.ids.AliasId;
 
-/// Destroy an alias output. Outputs controlled by it will be sweeped before if they don't have a
-/// storage deposit return, timelock or expiration unlock condition. The amount and possible native tokens will be
-/// sent to the governor address.
+/// Destroy an alias output.
 public class DestroyAlias implements AccountMethod {
 
     private AliasId aliasId;
