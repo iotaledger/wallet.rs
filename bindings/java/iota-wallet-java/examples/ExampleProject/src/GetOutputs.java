a487abac0cf2c1dff1bcbf72134eff3b0d0040e8
samuel_rufi <samuel.rufinatscha@iota.org>
2022-09-28 11:08:39 +0200

    Wire more examples with documentation

--- bindings/java/iota-wallet-java/examples/ExampleProject/src/GetOutputs.java
+++ bindings/java/iota-wallet-java/examples/ExampleProject/src/GetOutputs.java
@@ -3,12 +3,12 @@
 
 import org.iota.Wallet;
 import org.iota.types.*;
-import org.iota.types.account_methods.GetTransaction;
+import org.iota.types.account_methods.GetOutput;
+import org.iota.types.account_methods.Outputs;
 import org.iota.types.exceptions.WalletException;
-import org.iota.types.ids.account.AccountAlias;
 import org.iota.types.secret.StrongholdSecretManager;
 
-public class ListAndGetTransactions {
+public class GetOutputs {
     private static final String DEFAULT_DEVELOPMENT_MNEMONIC = "hidden enroll proud copper decide negative orient asset speed work dolphin atom unhappy game cannon scheme glow kid ring core name still twist actor";
 
     public static void main(String[] args) throws WalletException, InterruptedException {
@@ -23,25 +23,17 @@ public class ListAndGetTransactions {
         // Set up an account for this example.
         AccountHandle a = ExampleUtils.setUpAccountWithFunds(wallet, "Hans");
 
-        // Set up a transaction for this example.
-        AccountAddress address = wallet.getAccount(new AccountAlias("Hans")).getPublicAddresses()[0];
-        a.sendAmount(new org.iota.types.account_methods.SendAmount().withAddressesWithAmount(
-                new AddressWithAmount[]{new AddressWithAmount().withAddress(address.getAddress()).withAmount("1000000")}
-        ));
+        // Get outputs
+        OutputData[] outputs = a.getOutputs(new Outputs());
 
-        // List transactions
-        Transaction[] transactions = a.getTransactions();
+        // Print outputs
+        for (OutputData o : outputs)
+            System.out.println(o);
 
-        // Print transactions
-        for (Transaction tx : transactions)
-            System.out.println(tx.toString());
+        // Get a specific output by id
+        OutputData o = a.getOutput(new GetOutput().withOutputId(outputs[0].getOutputId()));
 
-        // Get a specific transaction
-        Transaction transaction = a.getTransaction(new GetTransaction().withTransactionId(transactions[0].getTransactionId()));
-
-        // Print transaction
-        System.out.println(transaction);
+        // Print the output
+        System.out.println(o);
     }
-
 }
-
