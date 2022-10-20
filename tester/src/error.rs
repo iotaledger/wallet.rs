4886c65068f9a4aee018f3e7cd51c1af74ff7a71
Thibault Martinez <thibault@iota.org>
2022-10-20 13:24:34 +0200

    Create account manager

--- tester/src/error.rs
+++ tester/src/error.rs
@@ -6,10 +6,16 @@ use std::fmt::Debug;
 /// The wallet-tester error type.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    /// Client error.
+    #[error("client error: {0}")]
+    Client(#[from] iota_wallet::iota_client::Error),
     /// I/O error.
     #[error("i/o error: {0}")]
     Io(#[from] std::io::Error),
     /// Json error.
     #[error("json error: {0}")]
     Json(#[from] serde_json::Error),
+    /// Wallet error.
+    #[error("wallet error: {0}")]
+    Wallet(#[from] iota_wallet::Error),
 }
--- tester/src/main.rs
+++ tester/src/main.rs
@@ -3,47 +3,75 @@
 
 mod error;
 
+use iota_wallet::{
+    account_manager::AccountManager,
+    iota_client::{
+        constants::SHIMMER_COIN_TYPE,
+        secret::{mnemonic::MnemonicSecretManager, SecretManager},
+    },
+    ClientOptions,
+};
 use serde_json::Value;
 use tokio::fs;
 
 use self::error::Error;
 
-fn process_fixtures(fixtures: &Value) -> Result<(), Error> {
+fn process_fixtures(_account_manager: &AccountManager, fixtures: &Value) -> Result<(), Error> {
     println!("{}", fixtures);
 
     Ok(())
 }
 
-fn process_transactions(transactions: &Value) -> Result<(), Error> {
+fn process_transactions(_account_manager: &AccountManager, transactions: &Value) -> Result<(), Error> {
     println!("{}", transactions);
 
     Ok(())
 }
 
-fn process_tests(tests: &Value) -> Result<(), Error> {
+fn process_tests(_account_manager: &AccountManager, tests: &Value) -> Result<(), Error> {
     println!("{}", tests);
 
     Ok(())
 }
 
-fn process_json(json: Value) -> Result<(), Error> {
+fn process_json(account_manager: &AccountManager, json: Value) -> Result<(), Error> {
     if let Some(fixtures) = json.get("fixtures") {
-        process_fixtures(fixtures)?;
+        process_fixtures(account_manager, fixtures)?;
     }
 
     if let Some(transactions) = json.get("transactions") {
-        process_transactions(transactions)?;
+        process_transactions(account_manager, transactions)?;
     }
 
     if let Some(tests) = json.get("tests") {
-        process_tests(tests)?;
+        process_tests(account_manager, tests)?;
     }
 
     Ok(())
 }
 
+async fn account_manager() -> Result<AccountManager, Error> {
+    let mnemonic = "pumpkin actual foster argue normal dizzy sheriff action license hover fossil pink ancient company toilet silver egg chief actress month family dose orange corn";
+    let secret_manager = MnemonicSecretManager::try_from_mnemonic(mnemonic)?;
+
+    let client_options = ClientOptions::new()
+        .with_node("https://api.testnet.shimmer.network")?
+        .with_node_sync_disabled();
+
+    let account_manager = AccountManager::builder()
+        .with_secret_manager(SecretManager::Mnemonic(secret_manager))
+        .with_client_options(client_options)
+        .with_coin_type(SHIMMER_COIN_TYPE)
+        .finish()
+        .await?;
+
+    Ok(account_manager)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let account_manager = account_manager().await?;
+
     let mut dir = fs::read_dir("json").await?;
 
     for entry in dir.next_entry().await? {
@@ -52,7 +80,7 @@ async fn main() -> Result<(), Error> {
 
         println!("{:?}", entry.file_name());
         println!("{}", json);
-        process_json(json)?;
+        process_json(&account_manager, json)?;
     }
 
     Ok(())
