49da6b1866162a5e46e99a67e92436e001e47a24
Thibault Martinez <thibault@iota.org>
2022-10-20 12:48:25 +0200

    Read all json files

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -1,4 +1,19 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {}
+use tokio::fs;
+
+#[tokio::main]
+async fn main() {
+    // TODO unwrap
+    let mut dir = fs::read_dir("json").await.unwrap();
+
+    // TODO unwrap
+    for entry in dir.next_entry().await.unwrap() {
+        // TODO unwrap
+        let content = fs::read_to_string(entry.path()).await.unwrap();
+
+        println!("{:?}", entry.file_name());
+        println!("{}", content);
+    }
+}
