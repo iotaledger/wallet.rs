1eb990b5c4626bd67e436af9530d60ab4541e1a2
Thibault Martinez <thibault@iota.org>
2022-10-20 12:51:01 +0200

    Json parsing

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -1,6 +1,7 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+use serde_json::Value;
 use tokio::fs;
 
 #[tokio::main]
@@ -12,8 +13,10 @@ async fn main() {
     for entry in dir.next_entry().await.unwrap() {
         // TODO unwrap
         let content = fs::read_to_string(entry.path()).await.unwrap();
+        // TODO unwrap
+        let json: Value = serde_json::from_str(&content).unwrap();
 
         println!("{:?}", entry.file_name());
-        println!("{}", content);
+        println!("{}", json);
     }
 }
