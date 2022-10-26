5e4dc62371db35d82028601d6c108af8ddd14dd4
Thibault Martinez <thibault@iota.org>
2022-10-26 11:49:06 +0200

    Start allowing other types of account state

--- tester/src/accounts/account.rs
+++ tester/src/accounts/account.rs
@@ -1,38 +1,15 @@
 // Copyright 2022 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-use iota_wallet::AddressWithAmount;
 use serde_json::Value;
 
-use crate::{context::Context, error::Error};
+use crate::{accounts::amount::process_amount, context::Context, error::Error};
 
 pub async fn process_account<'a>(context: &Context<'a>, account: &Value) -> Result<(), Error> {
-    let amount = if let Some(amount) = account.as_u64() {
-        amount
-    } else {
-        return Err(Error::InvalidField("account"));
-    };
+    let created_account = context.account_manager.create_account().finish().await?;
 
-    let account = context.account_manager.create_account().finish().await?;
-
-    if amount != 0 {
-        let transaction = context
-            .faucet_account
-            .send_amount(
-                vec![AddressWithAmount {
-                    address: account.addresses().await?[0].address().to_bech32(),
-                    amount: amount,
-                }],
-                None,
-            )
-            .await?;
-
-        if let Some(block_id) = transaction.block_id {
-            context
-                .faucet_account
-                .retry_until_included(&block_id, Some(1), None)
-                .await?;
-        }
+    if account.is_u64() {
+        process_amount(context, created_account, account.as_u64().unwrap()).await?;
     }
 
     Ok(())
--- tester/src/accounts/mod.rs
+++ tester/src/accounts/mod.rs
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 mod account;
+mod amount;
 
 use serde_json::Value;
 
