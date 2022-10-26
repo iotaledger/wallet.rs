0bbb38a4b45842cfc550da28df4d498abe706333
Tuditi <daviddetroch@pm.me>
2022-10-26 17:48:07 +0200

    fix: first example

--- bindings/nodejs/examples/account-manager.js
+++ bindings/nodejs/examples/account-manager.js
@@ -4,6 +4,13 @@ const { AccountManager } = require('@iota/wallet');
 async function getUnlockedManager() {
     const manager = new AccountManager({
         storagePath: './alice-database',
+        clientOptions: {
+            nodes: ['https://api.testnet.shimmer.network']
+        },
+        coinType: 4219,
+        secretManager: {
+            mnemonic: process.env.MNEMONIC
+        }      
     });
     await manager.setStrongholdPassword(process.env.SH_PASSWORD);
     return manager;
