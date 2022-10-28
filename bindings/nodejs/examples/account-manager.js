fb9a58d64a8625a098df30143939de789c140f4e
Tuditi <45079109+Tuditi@users.noreply.github.com>
2022-10-28 11:28:18 +0200

    fix: first example (#1514)
    * fix: first example
    
    * fix: align accountManagerOptions with first example

--- bindings/nodejs/examples/account-manager.js
+++ bindings/nodejs/examples/account-manager.js
@@ -1,9 +1,20 @@
 require('dotenv').config({ path: '../.env' });
-const { AccountManager } = require('@iota/wallet');
+const { AccountManager, CoinType } = require('@iota/wallet');
 
 async function getUnlockedManager() {
     const manager = new AccountManager({
         storagePath: './alice-database',
+        clientOptions: {
+            nodes: ['https://api.testnet.shimmer.network'],
+            localPow: true,
+        },
+        coinType: CoinType.Shimmer,
+        secretManager: {
+            Stronghold: {
+                snapshotPath: `./wallet.stronghold`,
+                password: `${process.env.SH_PASSWORD}`,
+            },
+        },
     });
     await manager.setStrongholdPassword(process.env.SH_PASSWORD);
     return manager;
