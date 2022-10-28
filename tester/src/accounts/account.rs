f172ff31a0a94512f69dc87e2049ab391fd38a8d
Thibault Martinez <thibault@iota.org>
2022-10-28 16:10:02 +0200

    Fix thoralf lint

--- tester/src/accounts/account.rs
+++ tester/src/accounts/account.rs
@@ -9,13 +9,13 @@ use crate::{
     error::Error,
 };
 
-pub async fn process_account<'a>(context: &Context<'a>, account: &Value) -> Result<(), Error> {
+pub async fn process_account<'a>(context: &Context<'a>, outputs: &Value) -> Result<(), Error> {
     let created_account = context.account_manager.create_account().finish().await?;
 
-    if account.is_u64() {
-        process_amount(context, &created_account, account.as_u64().unwrap()).await?;
-    } else if account.is_array() {
-        process_outputs(context, &created_account, account.as_array().unwrap()).await?;
+    if outputs.is_u64() {
+        process_amount(context, &created_account, outputs.as_u64().unwrap()).await?;
+    } else if outputs.is_array() {
+        process_outputs(context, &created_account, outputs.as_array().unwrap()).await?;
     }
 
     Ok(())
