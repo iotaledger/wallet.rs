8511cecc2ea6734990c7988ec56c3ab170b297e7
Brord van Wierst <brord@iota.org>
2022-11-07 23:14:49 -0300

    Fixed CheckBalance

--- bindings/java/iota-wallet-java/examples/src/main/CheckBalance.java
+++ bindings/java/iota-wallet-java/examples/src/main/CheckBalance.java
@@ -14,13 +14,13 @@ public class CheckBalance {
         // If you haven't run the ´CreateAccount.java´ example yet, you must run it first to be able to load the wallet as shown below:
         Wallet wallet = new Wallet(new WalletConfig()
                 .withClientOptions(new ClientConfig().withNodes(Env.NODE))
-                .withSecretManager(new StrongholdSecretManager("PASSWORD_FOR_ENCRYPTION2", null, "example-wallet"))
+                .withSecretManager(new StrongholdSecretManager(Env.PASSWORD, null, Env.SNAPSHOT_PATH))
                 .withCoinType(CoinType.Shimmer)
         );
 
         // Get account and sync it with the registered node to ensure that its balances are up-to-date.
         AccountHandle a = wallet.getAccount(new AccountAlias(Env.ACCOUNT_NAME));
-        // a.syncAccount(new SyncAccount().withOptions(new SyncOptions()));
+        a.syncAccount(new SyncAccount().withOptions(new SyncOptions()));
 
         // Get the balance.
         AccountBalance balance = a.getBalance();
