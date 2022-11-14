6107eb6cb551dbe14aa93e26d85387b0268b4e05
Brord van Wierst <brord@iota.org>
2022-11-14 18:06:32 -0300

    fixed NewOuput event

--- bindings/java/iota-wallet-java/lib/src/main/java/org/iota/types/events/wallet/NewOutput.java
+++ bindings/java/iota-wallet-java/lib/src/main/java/org/iota/types/events/wallet/NewOutput.java
@@ -3,19 +3,31 @@
 
 package org.iota.types.events.wallet;
 
-import java.util.Optional;
-
 import org.iota.types.OutputData;
+import org.iota.types.OutputResponse;
 import org.iota.types.payload.TransactionPayload;
 
 public class NewOutput extends WalletEvent {
+
     /// The new output.
-    OutputData output;
+    private OutputData output;
 
     /// The transaction that created the output. Might be pruned and not available.
-    Optional<TransactionPayload> transaction;
+    private TransactionPayload transaction;
 
     /// The inputs for the transaction that created the output. Might be pruned and
     /// not available.
-    // Optional<[OutputWithMetadataResponse]> transactionInputs;
+    private OutputResponse[] transactionInputs;
+
+    public OutputData getOutput() {
+        return output;
+    }
+
+    public TransactionPayload getTransaction() {
+        return transaction;
+    }
+
+    public OutputResponse[] getTransactionInputs() {
+        return transactionInputs;
+    }
 }
