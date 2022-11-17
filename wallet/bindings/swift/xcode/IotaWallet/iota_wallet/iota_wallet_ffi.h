47f8fc06cac6a24a7ca48eb3f8659844ffcab300
Brord van Wierst <brord@iota.org>
2022-11-17 18:32:00 -0300

    removed nullable and format on .h file

--- wallet/bindings/swift/xcode/IotaWallet/iota_wallet/iota_wallet_ffi.h
+++ wallet/bindings/swift/xcode/IotaWallet/iota_wallet/iota_wallet_ffi.h
@@ -20,7 +20,6 @@ extern void iota_destroy(iota_wallet_handle_t*);
 extern void iota_send_message(iota_wallet_handle_t* wallet_handle, const char* message, Callback callback, void* context);
 extern int8_t iota_listen(iota_wallet_handle_t* wallet_handle, const char* event_types, Callback callback, void* context, char* error_buffer, size_t error_buffer_size);
 
-
 #ifdef __cplusplus
 }
 #endif
