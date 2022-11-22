ce83deb38a80ff257637124a8a6ac0d0edc304e2
Samuel Rufinatscha <samuel.rufinatscha@iota.org>
2022-11-22 14:39:33 +0100

    Add InitializeWalletException (#1602)

---
+++ bindings/java/iota-wallet-java/lib/src/main/java/org/iota/types/exceptions/InitializeWalletException.java
@@ -0,0 +1,12 @@
+// Copyright 2022 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
+package org.iota.types.exceptions;
+
+public class InitializeWalletException extends Exception {
+
+    public InitializeWalletException(String message) {
+        super(message);
+    }
+
+}
