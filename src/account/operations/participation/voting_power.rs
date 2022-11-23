d7337375f3aba0e4a5506a42373ba95eb8c736f9
Thibault Martinez <thibault@iota.org>
2022-11-23 12:53:18 +0100

    Rework voting power

--- src/account/operations/participation/voting_power.rs
+++ src/account/operations/participation/voting_power.rs
@@ -6,7 +6,7 @@ use iota_client::{
         output::{
             feature::{MetadataFeature, TagFeature},
             unlock_condition::AddressUnlockCondition,
-            BasicOutputBuilder, Feature, Output, UnlockCondition,
+            BasicOutput, BasicOutputBuilder, Feature, Output, UnlockCondition,
         },
         payload::TaggedDataPayload,
     },
@@ -19,61 +19,44 @@ use crate::{
 };
 
 impl AccountHandle {
+    // TODO Should this return Option ? Or 0 voting power in case of absence of output?
     /// Returns an account's total voting power (voting or NOT voting).
     pub async fn get_voting_power(&self) -> Result<u64> {
         let voting_output = self.get_voting_output().await?;
+
         Ok(voting_output.output.amount())
     }
 
     /// Designates a given amount of tokens towards an account's "voting power" by creating a
     /// special output, which is really a basic one with some metadata.
     ///
-    /// If not enough funds, throw error
-    /// If voting use voting output (should only ever have one unless more spaces for more votes is needed)
-    /// Remove metadata for any events that have expired (use event IDs to get cached event information, check event
-    /// milestones in there against latest network milestone) Prioritize consuming outputs that are designated for
-    /// voting but don't have any metadata (only possible if user increases voting power then increases again
-    /// immediately after)
+    /// If not enough funds, throws an error.
+    /// If voting, use voting output (should only ever have one unless more space for more votes is needed).
+    /// Removes metadata for any events that have expired (uses event IDs to get cached event information, checks event
+    /// milestones in there against latest network milestone).
+    /// Prioritizes consuming outputs that are designated for voting but don't have any metadata (only possible if user
+    /// increases voting power then increases again immediately after).
     pub async fn increase_voting_power(&self, amount: u64) -> Result<Transaction> {
         let token_supply = self.client().get_token_supply().await?;
 
         let (new_voting_output, tx_options) = match self.get_voting_output().await {
-            Ok(previous_voting_output_data) => {
-                let basic_output = if let Output::Basic(basic_output) = previous_voting_output_data.output {
-                    basic_output
+            Ok(current_voting_output_data) => {
+                let output = if let Output::Basic(output) = current_voting_output_data.output {
+                    output
                 } else {
                     unreachable!("voting output needs to be a basic output")
                 };
 
-                let mut updated_output_builder =
-                    BasicOutputBuilder::from(&basic_output).with_amount(basic_output.amount() + amount)?;
-
-                let mut participation_data = basic_output.features().metadata().map(|m| m.data()).unwrap_or(&[]);
-
-                let participation_data =
-                    if let Ok(mut participations) = Participations::from_bytes(&mut participation_data) {
-                        // Remove ended participations
-                        self.remove_ended_participation_events(&mut participations).await?;
-
-                        let participation_bytes = participations.to_bytes()?;
-
-                        updated_output_builder = updated_output_builder
-                            .replace_feature(Feature::Metadata(MetadataFeature::new(participation_bytes.clone())?))?;
-
-                        participation_bytes
-                    } else {
-                        vec![]
-                    };
+                let (new_output, tagged_data_payload) = self
+                    .new_voting_output(&output, output.amount() + amount, token_supply)
+                    .await?;
 
                 (
-                    updated_output_builder.finish_output(token_supply)?,
+                    new_output,
                     Some(TransactionOptions {
-                        // Use the previous voting output and additionally other for the additional amount
-                        mandatory_inputs: Some(vec![previous_voting_output_data.output_id]),
-                        tagged_data_payload: Some(TaggedDataPayload::new(
-                            PARTICIPATION_TAG.as_bytes().to_vec(),
-                            participation_data.to_vec(),
-                        )?),
+                        // Use the previous voting output and additionally other for the additional amount.
+                        mandatory_inputs: Some(vec![current_voting_output_data.output_id]),
+                        tagged_data_payload: Some(tagged_data_payload),
                         ..Default::default()
                     }),
                 )
@@ -99,71 +82,80 @@ impl AccountHandle {
 
     /// Reduces an account's "voting power" by a given amount.
     ///
-    /// If amount is higher than actual voting power, throw error
-    /// If voting and amount is equal to voting power, remove tagged data payload and output metadata
-    /// Remove metadata for any events that have expired (use event IDs to get cached event information, check event
-    /// milestones in there against latest network milestone) If an output is designated for voting but doesn't
-    /// contain Prioritize consuming outputs that are designated for voting but don't have any metadata (only
-    /// possible if user increases voting power then decreases immediately after)
+    /// If amount is higher than actual voting power, throws an error.
+    /// If voting and amount is equal to voting power, removes tagged data payload and output metadata.
+    /// Removes metadata for any events that have expired (uses event IDs to get cached event information, checks event
+    /// milestones in there against latest network milestone).
+    /// If an output is designated for voting but doesn't contain
+    /// Prioritizes consuming outputs that are designated for voting but don't have any metadata (only possible if user
+    /// increases voting power then decreases immediately after).
     pub async fn decrease_voting_power(&self, amount: u64) -> Result<Transaction> {
         let token_supply = self.client().get_token_supply().await?;
 
-        let previous_voting_output_data = self.get_voting_output().await?;
+        let current_voting_output_data = self.get_voting_output().await?;
 
-        let basic_output = if let Output::Basic(basic_output) = previous_voting_output_data.output {
-            basic_output
+        let output = if let Output::Basic(output) = current_voting_output_data.output {
+            output
         } else {
             unreachable!("voting output needs to be a basic output")
         };
 
         // If the amount to decrease is the amount of the output, then we just remove the features
-        let (new_output, tagged_data_payload) = if amount == basic_output.amount() {
+        let (new_output, tagged_data_payload) = if amount == output.amount() {
             (
-                vec![BasicOutputBuilder::from(&basic_output)
+                BasicOutputBuilder::from(&output)
                     .with_features([])
-                    .finish_output(token_supply)?],
+                    .finish_output(token_supply)?,
                 None,
             )
         } else {
-            let mut participation_data = basic_output.features().metadata().map(|m| m.data()).unwrap_or(&[]);
+            let (new_output, tagged_data_payload) = self
+                .new_voting_output(&output, output.amount() - amount, token_supply)
+                .await?;
 
-            let mut updated_output_builder =
-                BasicOutputBuilder::from(&basic_output).with_amount(basic_output.amount() - amount)?;
-
-            let participation_data = if let Ok(mut participations) = Participations::from_bytes(&mut participation_data)
-            {
-                // Remove ended participations
-                self.remove_ended_participation_events(&mut participations).await?;
-
-                let participation_bytes = participations.to_bytes()?;
-
-                updated_output_builder = updated_output_builder
-                    .replace_feature(Feature::Metadata(MetadataFeature::new(participation_bytes.clone())?))?;
-
-                participation_bytes
-            } else {
-                vec![]
-            };
-
-            (
-                vec![updated_output_builder.finish_output(token_supply)?],
-                Some(TaggedDataPayload::new(
-                    PARTICIPATION_TAG.as_bytes().to_vec(),
-                    participation_data.to_vec(),
-                )?),
-            )
+            (new_output, Some(tagged_data_payload))
         };
 
         self.send(
-            new_output,
+            vec![new_output],
             Some(TransactionOptions {
                 // Use the previous voting output and additionally others for possible additional required amount for
                 // the remainder to reach the minimum required storage deposit
-                mandatory_inputs: Some(vec![previous_voting_output_data.output_id]),
+                mandatory_inputs: Some(vec![current_voting_output_data.output_id]),
                 tagged_data_payload,
                 ..Default::default()
             }),
         )
         .await
+    }
+
+    async fn new_voting_output(
+        &self,
+        output: &BasicOutput,
+        amount: u64,
+        token_supply: u64,
+    ) -> Result<(Output, TaggedDataPayload)> {
+        let mut output_builder = BasicOutputBuilder::from(output).with_amount(amount)?;
+
+        let mut participation_data = output.features().metadata().map(|m| m.data()).unwrap_or(&[]);
+
+        let participation_data = if let Ok(mut participations) = Participations::from_bytes(&mut participation_data) {
+            // Remove ended participations.
+            self.remove_ended_participation_events(&mut participations).await?;
+
+            let participation_bytes = participations.to_bytes()?;
+
+            output_builder = output_builder
+                .replace_feature(Feature::Metadata(MetadataFeature::new(participation_bytes.clone())?))?;
+
+            participation_bytes
+        } else {
+            vec![]
+        };
+
+        Ok((
+            output_builder.finish_output(token_supply)?,
+            TaggedDataPayload::new(PARTICIPATION_TAG.as_bytes().to_vec(), participation_data.to_vec())?,
+        ))
     }
 }
