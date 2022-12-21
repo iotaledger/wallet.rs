4d039ee0cfec386bdd5ca482d319517b20f10d74
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-12-21 15:21:14 +0100

    chore: update plugin definitions

--- bindings/capacitorjs/src/definitions.ts
+++ bindings/capacitorjs/src/definitions.ts
@@ -1,31 +1,47 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-import { PluginListenerHandle } from "@capacitor/core"
-import { MessageHandler } from "./lib/MessageHandler"
+// import { PluginListenerHandle } from "@capacitor/core"
+// import type { MessageHandler } from "./lib/MessageHandler"
 import type {
   EventType,
   __Message__,
   __AccountMethod__,
+  // AccountManagerOptions,
 } from './types'
 
-export * from './types'
+
 export interface IotaWalletMobileTypes {
   initLogger(path: string): Promise<void>
-  messageHandlerNew(options: string): Promise<void>
-  sendMessage(
+  
+  messageHandlerNew(messageOptions: { 
+    storagePath: string 
+  }): Promise<{ messageHandler: number }>
+  
+  sendMessage(messageOptions: {
     message: string, 
-    handler :MessageHandler, 
-    callback: (error: Error, result: string) => void
-  ): PluginListenerHandle
+    handler : number, 
+  }): Promise<{ result: string }>
+  
   listen(
-    eventTypes: EventType[], 
-    callback: (error: Error, result: string) => void,
-    messageHandler: any
-  ): void
-  clearListeners(
+    options: {
+      eventTypes: EventType[];
+      messageHandler: number;
+    },
+    callback: (message: {
+      error: {
+        cause: unknown;
+      };
+      result: string;
+    }) => void
+  ): Promise<string>;
+  
+  clearListeners(options: {
     eventTypes: EventType[],
-    messageHandler: any
-  ): void
-  destroy(messageHandler: any): void
+    messageHandler: number
+  }): Promise<{ result: string }>
+  
+  destroy(options: { 
+    messageHandler: number 
+  }): Promise<void>;
 }
