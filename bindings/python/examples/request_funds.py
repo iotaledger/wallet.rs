89c2189684ea02e196ac0d9940e2a97ce9a12769
Samuel Rufinatscha <samuel.rufinatscha@iota.org>
2023-01-09 14:39:58 +0100

    Add missing Python examples (#1709)
    * Complete Python examples
    
    * Remove unused import
    
    * Update bindings/python/examples/burn_nft.py
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * Update bindings/python/examples/destroy_foundry.py
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

---
+++ bindings/python/examples/request_funds.py
@@ -0,0 +1,28 @@
+from iota_wallet import IotaWallet
+
+import time
+
+# This example requests funds from the faucet
+
+wallet = IotaWallet('./alice-database')
+
+account = wallet.get_account('Alice')
+
+# Sync account with the node
+response = account.sync_account()
+print(f'Synced: {response}')
+
+# Balance before funding
+balance = account.get_balance()
+print(f'balance before faucet request: { balance[ "baseCoin" ][ "available" ] }')
+
+response = account.request_funds_from_faucet("https://faucet.testnet.shimmer.network/api/enqueue", "rms1qpszqzadsym6wpppd6z037dvlejmjuke7s24hm95s9fg9vpua7vluaw60xu")
+
+time.sleep(20)
+
+# Sync account with the node
+response = account.sync_account()
+
+# Balance after funding
+balance = account.get_balance()
+print(f'balance after faucet request: { balance[ "baseCoin" ][ "available" ] }')
