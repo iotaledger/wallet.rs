0f58a9d7a9196c1b994bc96aca9a5363aee7099b
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-01-11 13:09:01 +0100

    chore: update plugin definitions
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- bindings/capacitorjs/src/definitions.ts
+++ bindings/capacitorjs/src/definitions.ts
@@ -1,47 +1,44 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// import { PluginListenerHandle } from "@capacitor/core"
-// import type { MessageHandler } from "./lib/MessageHandler"
 import type {
   EventType,
   __Message__,
   __AccountMethod__,
-  // AccountManagerOptions,
+  AccountManagerOptions,
 } from './types'
 
 
 export interface IotaWalletMobileTypes {
-  initLogger(path: string): Promise<void>
-  
-  messageHandlerNew(messageOptions: { 
-    storagePath: string 
-  }): Promise<{ messageHandler: number }>
-  
-  sendMessage(messageOptions: {
-    message: string, 
-    handler : number, 
-  }): Promise<{ result: string }>
-  
-  listen(
-    options: {
-      eventTypes: EventType[];
-      messageHandler: number;
-    },
-    callback: (message: {
-      error: {
-        cause: unknown;
-      };
-      result: string;
-    }) => void
-  ): Promise<string>;
-  
-  clearListeners(options: {
-    eventTypes: EventType[],
-    messageHandler: number
-  }): Promise<{ result: string }>
-  
-  destroy(options: { 
-    messageHandler: number 
-  }): Promise<void>;
+    initLogger(path: string): Promise<void>;
+    messageHandlerNew(messageOptions: AccountManagerOptions): Promise<{
+        messageHandler: number;
+    }>;
+    sendMessage(messageOptions: {
+        message: string;
+        handler: number;
+    }): Promise<{
+        result: string;
+    }>;
+    listen(options: {
+        eventTypes: EventType[];
+        messageHandler: number;
+    }, callback: (message: {
+        error: {
+            cause: unknown;
+        };
+        result: string;
+    }) => void): Promise<string>;
+    clearListeners(options: {
+        eventTypes: EventType[];
+        messageHandler: number;
+    }, callback: (message: {
+        error: {
+            cause: unknown;
+        };
+        result: string;
+    }) => void): Promise<string>;
+    destroy(options: {
+        messageHandler: number;
+    }): Promise<void>;
 }
