ecad7fd429b5a82fcbe05fb9533572f7ae39083e
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-01-11 13:09:01 +0100

    chore: update plugin definitions
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- bindings/capacitorjs/src/index.ts
+++ bindings/capacitorjs/src/index.ts
@@ -1,12 +1,12 @@
+// Copyright 2021 IOTA Stiftung
+// SPDX-License-Identifier: Apache-2.0
+
 import { registerPlugin } from '@capacitor/core'
 
 import { IotaWalletMobileTypes } from './definitions'
 
 const IotaWalletMobile = registerPlugin<IotaWalletMobileTypes>('IotaWalletMobile')
 
-// import * as WalletApi from './lib/index.js'
-
-// IotaWalletMobile.WalletApi = WalletApi
-
 export * from './definitions'
+
 export { IotaWalletMobile }
