176ab3b9a04cdac70b14a4a7a742075cb72b687b
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-01-25 15:52:17 +0100

    chore: polish js wallet api
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- bindings/capacitorjs/src/lib/bindings.ts
+++ bindings/capacitorjs/src/lib/bindings.ts
@@ -1,13 +1,7 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// import type { MessageHandler } from './MessageHandler'
-// import { IotaWalletMobileTypes } from "@iota/wallet-mobile"
-// import { registerPlugin } from '@capacitor/core'
-
-// const IotaWalletMobile = registerPlugin<IotaWalletMobileTypes>('IotaWalletMobile')
 import { IotaWalletMobile } from '../index'
-// const IotaWalletMobile: IotaWalletMobileTypes
 
 const {
     initLogger,
