aa22e1e10af93bd00f2f0e6d121010565ee8d877
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-01-25 13:05:08 +0100

    chore: clean js api code
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- bindings/capacitorjs/src/lib/Account.ts
+++ bindings/capacitorjs/src/lib/Account.ts
@@ -1,7 +1,6 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
-// import type { MessageHandler } from './MessageHandler';
 import type {
     AccountBalance,
     AccountMetadata,
