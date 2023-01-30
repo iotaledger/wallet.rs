646be9b13f775349a693b0aafca7127356437065
Tuditi <daviddetroch@pm.me>
2023-01-30 15:45:19 +0100

    chore: remove unusude import

--- bindings/nodejs/lib/Account.ts
+++ bindings/nodejs/lib/Account.ts
@@ -28,7 +28,6 @@ import type {
     Transaction,
     TransactionOptions,
     ParticipationOverview,
-    ParticipationEvent,
     ParticipationEventId,
     ParticipationEventStatus,
     ParticipationEventType,
