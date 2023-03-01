1bb1f0e0902492db92d4e05754a2801f3434da54
Thibault Martinez <thibault@iota.org>
2023-03-01 14:23:57 +0100

    Fmt

--- wallet/bindings/nodejs/types/bridge/account.ts
+++ wallet/bindings/nodejs/types/bridge/account.ts
@@ -393,7 +393,7 @@ export type __GetParticipationEventMethod__ = {
 export type __GetParticipationEventIdsMethod__ = {
     name: 'getParticipationEventIds';
     data: {
-        node: Node,
+        node: Node;
         eventType?: ParticipationEventType;
     };
 };
