51bebab70a7082ff4d9d440c987d7a7a27402ca3
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-03-13 12:32:49 +0000

    temporarily using host https://files.iota.org/firefly/bindings until CI will be done
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- wallet/bindings/capacitorjs/add_libs.sh
+++ wallet/bindings/capacitorjs/add_libs.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
+# 🛑 temporarily using host https://files.iota.org/firefly/bindings until CI will be done.
 set -e
 rm -rf tmp && mkdir tmp && cd tmp
-
-curl -SL --progress-bar --fail https://github.com/iotaledger/wallet.rs/releases/download/iota-wallet-java-1.0.0-rc.1-new/iota-wallet-1.0.0-rc.1-android.zip > iota-wallet.zip
-unzip iota-wallet.zip             
-
+echo Installing Java libraries
+# curl -SL --progress-bar --fail https://github.com/iotaledger/wallet.rs/releases/download/iota-wallet-java-1.0.0-rc.1-new/iota-wallet-1.0.0-rc.1-android.zip > iota-wallet-java.zip
+mkdir jniLibs && cd jniLibs
+curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/jniLibs-1.0.0-alpha.0.zip > iota-wallet-java.zip
+unzip iota-wallet-java.zip             
 rm -rf ../android/src/main/jniLibs
-cp -r jniLibs ../android/src/main/jniLibs
-
-curl -SL --progress-bar --fail https://github.com/iotaledger/wallet.rs/releases/download/iota-wallet-java-1.0.0-rc.1-new/iota-wallet-1.0.0-rc.1.jar > iota-wallet.jar
+cd .. && cp -rv jniLibs ../android/src/main
+curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/iota-wallet-1.0.0-alpha.0.jar > iota-wallet.jar
 rm -rf ../android/libs && mkdir -p ../android/libs             
-cp -r iota-wallet.jar ../android/libs
-
+cp -rv iota-wallet.jar ../android/libs
+echo Installing Swift libraries
+curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/WalletFramework-2.0.0-alpha.0.zip > iota-wallet-swift.zip
+unzip iota-wallet-swift.zip             
+rm -f ../ios/WalletFramework.xcframework/ios-x86/WalletFramework.framework/WalletFramework
+rm -f ../ios/WalletFramework.xcframework/ios-arm64/WalletFramework.framework/WalletFramework
+cp -v x86_64/libiota_wallet.a ../ios/WalletFramework.xcframework/ios-x86/WalletFramework.framework/WalletFramework
+cp -v aarch64/libiota_wallet.a ../ios/WalletFramework.xcframework/ios-arm64/WalletFramework.framework/WalletFramework
 cd .. && rm -rf tmp
-echo "success!"
