434edd4b7c0ca26b3ed2eeaf59122f327b618d6a
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-03-16 19:03:01 +0000

    fix script copy libraries
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- wallet/bindings/capacitorjs/add_libs.sh
+++ wallet/bindings/capacitorjs/add_libs.sh
@@ -4,11 +4,10 @@ set -e
 rm -rf tmp && mkdir tmp && cd tmp
 echo Installing Java libraries
 # curl -SL --progress-bar --fail https://github.com/iotaledger/wallet.rs/releases/download/iota-wallet-java-1.0.0-rc.1-new/iota-wallet-1.0.0-rc.1-android.zip > iota-wallet-java.zip
-mkdir jniLibs && cd jniLibs
 curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/jniLibs-1.0.0-alpha.0.zip > iota-wallet-java.zip
 unzip iota-wallet-java.zip             
 rm -rf ../android/src/main/jniLibs
-cd .. && cp -rv jniLibs ../android/src/main
+cp -rv jniLibs ../android/src/main
 curl -SL --progress-bar --fail https://files.iota.org/firefly/bindings/iota-wallet-1.0.0-alpha.0.jar > iota-wallet.jar
 rm -rf ../android/libs && mkdir -p ../android/libs             
 cp -rv iota-wallet.jar ../android/libs
