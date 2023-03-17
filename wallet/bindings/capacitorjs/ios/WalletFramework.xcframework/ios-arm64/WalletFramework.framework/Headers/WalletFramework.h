65da59ccdd0a2fa7df61c082c451ae2b995ae912
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-03-17 12:53:24 +0000

    Remove `clearListeners` from `WalletFramework.h`
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- wallet/bindings/capacitorjs/ios/WalletFramework.xcframework/ios-arm64/WalletFramework.framework/Headers/WalletFramework.h
+++ wallet/bindings/capacitorjs/ios/WalletFramework.xcframework/ios-arm64/WalletFramework.framework/Headers/WalletFramework.h
@@ -18,7 +18,6 @@ extern iota_wallet_handle_t* iota_initialize(const char* manager_options, char* 
 extern void iota_destroy(iota_wallet_handle_t*);
 extern void iota_send_message(iota_wallet_handle_t* wallet_handle, const char* message, Callback callback, void* context);
 extern int8_t iota_listen(iota_wallet_handle_t* wallet_handle, const char* event_types, Callback callback, void* context, char* error_buffer, size_t error_buffer_size);
-extern int8_t iota_clear_listeners(iota_wallet_handle_t* wallet_handle, const char* event_types, Callback callback, void* context, char* error_buffer, size_t error_buffer_size);
 
 
 #ifdef __cplusplus
